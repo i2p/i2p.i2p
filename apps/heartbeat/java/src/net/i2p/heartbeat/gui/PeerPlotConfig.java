ba8fb23b9da488eaa0a2db521932025d547fe5f7
jrandom <jrandom>
2004-04-10 09:43:29 +0000

    editor testing (ignore for now)

---
+++ apps/heartbeat/java/src/net/i2p/heartbeat/gui/PeerPlotConfig.java
@@ -0,0 +1,20 @@
+package net.i2p.heartbeat.gui;
+
+import net.i2p.heartbeat.ClientConfig;
+
+/**
+ * Configure how we want to render a particular clientConfig in the GUI
+ *
+ */
+class PeerPlotConfig {
+    private ClientConfig _config;
+    
+    private final static void foo() {
+	// bar
+	if (true) {
+	    // baz
+	}
+	// baf
+    } 
+    // moo
+}
