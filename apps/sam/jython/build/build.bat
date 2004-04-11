a7597b2a6d2ce495a4ee442ea22533ed5c3ef60f
aum <aum>
2004-04-11 10:31:41 +0000

    Tested build process on windows. Updated README, and fixed build.bat, so windows build of i2psam.jar works.

--- apps/sam/jython/build/build.bat
+++ apps/sam/jython/build/build.bat
@@ -1,11 +1,11 @@
 @echo off
 rem This makefile constructs the SAM Server i2psam.jar
-
-rem by default, it gets linked into a standalone .jar file, together
-rem with the I2P core classes, and the jython classes
-
-CLASSPATH=\path\to\my\jython.jar;..\..\..\..\build\i2p.jar;..\..\..\..\build\mstreaming.jar
+rem Once you've built i2psam.jar, you'll need to copy it and
+rem jython.jar into the main jars directory
 
 @echo Building i2psam.jar
-jythonc --jar i2psam.jar --all ..\src\i2psam.py
+@echo on
+set CLASSPATH=..\..\..\..\build\i2p.jar;..\..\..\..\build\mstreaming.jar
 
+@echo Building i2psam.jar
+jythonc --jar i2psam.jar ..\src\i2psam.py
