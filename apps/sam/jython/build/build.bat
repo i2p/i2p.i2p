d3ad111b85c9ff19e52029996ce652ad5da8806c
aum <aum>
2004-04-12 06:41:24 +0000

    change - now goes to root build dir and does 'ant build' to ensure the required jars are present

--- apps/sam/jython/build/build.bat
+++ apps/sam/jython/build/build.bat
@@ -3,6 +3,11 @@ rem This makefile constructs the SAM Server i2psam.jar
 rem Once you've built i2psam.jar, you'll need to copy it and
 rem jython.jar into the main jars directory
 
+@echo Ensuring i2p jars are built
+cd ..\..\..\..
+ant build
+cd apps\sam\jython\build
+
 @echo Building i2psam.jar
 @echo on
 set CLASSPATH=..\..\..\..\build\i2p.jar;..\..\..\..\build\mstreaming.jar
