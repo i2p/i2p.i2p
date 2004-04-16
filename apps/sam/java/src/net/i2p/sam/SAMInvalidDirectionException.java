58c145ba0861ca39f0675f833ada29238399e252
human <human>
2004-04-16 03:44:35 +0000

    Oops!  Added missing file... (human)

---
+++ apps/sam/java/src/net/i2p/sam/SAMInvalidDirectionException.java
@@ -0,0 +1,26 @@
+package net.i2p.sam;
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by human in 2004 and released into the public domain 
+ * with no warranty of any kind, either expressed or implied.  
+ * It probably won't  make your computer catch on fire, or eat 
+ * your children, but it might.  Use at your own risk.
+ *
+ */
+
+/**
+ * Exception thrown by SAM methods when an application tries to create outgoing
+ * connections through a receive-only SAM session.
+ *
+ * @author human
+ */
+public class SAMInvalidDirectionException extends Exception {
+
+    public SAMInvalidDirectionException() {
+	super();
+    }
+    
+    public SAMInvalidDirectionException(String s) {
+	super(s);
+    }
+}
