bbcde2f52b6240f16acfed09393765a9a90893e3
jrandom <jrandom>
2004-07-15 01:08:54 +0000

    0.3.2.2 (a lil installation testing and then i'll push)

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -14,8 +14,8 @@ package net.i2p;
  *
  */
 public class CoreVersion {
-    public final static String ID = "$Revision: 1.10 $ $Date: 2004/07/07 12:58:37 $";
-    public final static String VERSION = "0.3.2.1";
+    public final static String ID = "$Revision: 1.11 $ $Date: 2004/07/11 13:57:02 $";
+    public final static String VERSION = "0.3.2.2";
 
     public static void main(String args[]) {
         System.out.println("I2P Core version: " + VERSION);
--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,8 +15,8 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.9 $ $Date: 2004/07/07 12:58:38 $";
-    public final static String VERSION = "0.3.2.1";
+    public final static String ID = "$Revision: 1.10 $ $Date: 2004/07/11 13:57:01 $";
+    public final static String VERSION = "0.3.2.2";
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION);
         System.out.println("Router ID: " + RouterVersion.ID);
