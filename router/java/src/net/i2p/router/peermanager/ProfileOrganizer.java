f06e21ff5a81307e08e59d2ea77459722ba1ac8d
jrandom <jrandom>
2004-07-20 04:10:33 +0000

    null check (oops)

--- router/java/src/net/i2p/router/peermanager/ProfileOrganizer.java
+++ router/java/src/net/i2p/router/peermanager/ProfileOrganizer.java
@@ -201,7 +201,10 @@ public class ProfileOrganizer {
             // we only use selectHighCapacityPeers when we are selecting for PURPOSE_TEST
             // or we are falling back due to _fastPeers being too small, so we can always 
             // exclude the fast peers
-            exclude.addAll(_fastPeers.keySet());
+            if (exclude == null)
+                exclude = new HashSet(_fastPeers.keySet());
+            else
+                exclude.addAll(_fastPeers.keySet());
             locked_selectPeers(_highCapacityPeers, howMany, exclude, matches);
         }
         if (matches.size() < howMany)
