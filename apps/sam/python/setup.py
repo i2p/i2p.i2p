f6d8d93a1b84a913e0ffab4108dab869690deff1
aum <aum>
2004-08-14 17:19:24 +0000

    Removed all stasher-specifics, relocated them to i2p/apps/stasher

--- apps/sam/python/setup.py
+++ apps/sam/python/setup.py
@@ -5,18 +5,11 @@ import os, sys
 
 os.chdir('./src')
 
-if sys.platform == 'win32':
-    stasherFrontEnd = "..\\stasher.py"
-else:
-    stasherFrontEnd = "../stasher"
-
 setup(name="Python I2P API",
       version="0.91",
       description="Python Interface to I2P",
-      author="Connelly Barnes + Aum",
+      author="Connelly Barnes",
       author_email="'Y29ubmVsbHliYXJuZXNAeWFob28uY29t\n'.decode('base64')",
       url="http://www.i2p.net/",
       packages=['i2p', 'i2p.pylib'],
-      py_modules=['bencode'],
-      scripts=[stasherFrontEnd],
      )
--- apps/sam/python/stasher.py
+++
@@ -1,3 +0,0 @@
-# wrapper script to run stasher node
-import i2p.stasher
-i2p.stasher.main()
