ea5662a4a2b47b7a4de3ab9163d358c793523b55
jrandom <jrandom>
2004-09-23 01:21:33 +0000

    another minor semantic jikes warning

--- router/java/src/net/i2p/router/Router.java
+++ router/java/src/net/i2p/router/Router.java
@@ -571,7 +571,7 @@ public class Router {
         try { _context.messageRegistry().shutdown(); } catch (Throwable t) { _log.log(Log.CRIT, "Error shutting down the message registry", t); }
         try { _context.messageValidator().shutdown(); } catch (Throwable t) { _log.log(Log.CRIT, "Error shutting down the message validator", t); }
         try { _sessionKeyPersistenceHelper.shutdown(); } catch (Throwable t) { _log.log(Log.CRIT, "Error shutting down the session key manager", t); }
-        _context.listContexts().remove(_context);
+        RouterContext.listContexts().remove(_context);
         dumpStats();
         try {
             for (Iterator iter = _shutdownTasks.iterator(); iter.hasNext(); ) {
