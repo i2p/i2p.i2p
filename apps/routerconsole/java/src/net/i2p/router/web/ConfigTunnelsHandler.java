9646ac2911660366e038892545be754a8fcfcbee
jrandom <jrandom>
2005-02-16 22:35:12 +0000

    continuing 0.5 merges

---
+++ apps/routerconsole/java/src/net/i2p/router/web/ConfigTunnelsHandler.java
@@ -0,0 +1,154 @@
+package net.i2p.router.web;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.i2p.data.Hash;
+import net.i2p.data.DataFormatException;
+import net.i2p.util.Log;
+import net.i2p.router.TunnelPoolSettings;
+
+/**
+ * Handler to deal with form submissions from the tunnel config form and act
+ * upon the values.  Holy crap, this is UUUUGLY
+ *
+ */
+public class ConfigTunnelsHandler extends FormHandler {
+    private Log _log;
+    private Map _settings;
+    private boolean _shouldSave;
+    
+    public ConfigTunnelsHandler() {
+        _shouldSave = false;
+    }
+    
+    protected void processForm() {
+        if (_shouldSave) {
+            saveChanges();
+        } else {
+            // noop
+        }
+    }
+    
+    public void setShouldsave(String moo) { 
+        if ( (moo != null) && (moo.equals("Save changes")) )
+            _shouldSave = true; 
+    }
+    
+    public void setSettings(Map settings) { _settings = new HashMap(settings); }
+    
+    /**
+     * The user made changes to the network config and wants to save them, so
+     * lets go ahead and do so.
+     *
+     */
+    private void saveChanges() {
+        _log = _context.logManager().getLog(ConfigTunnelsHandler.class);
+        boolean saveRequired = false;
+        
+        if (_log.shouldLog(Log.DEBUG))
+            _log.debug("Saving changes, with props = " + _settings);
+        
+        int updated = 0;
+        int index = 0;
+        while (true) {
+            Object val = _settings.get("pool." + index);
+            if (val == null) break;
+            Hash client = new Hash();
+            
+            String poolName = (val instanceof String ? (String)val : ((String[])val)[0]);
+            
+            TunnelPoolSettings in = null;
+            TunnelPoolSettings out = null;
+            if ("exploratory".equals(poolName)) {
+                in = _context.tunnelManager().getInboundSettings();
+                out = _context.tunnelManager().getOutboundSettings();
+            } else {
+                try {
+                    client.fromBase64(poolName);
+                } catch (DataFormatException dfe) {
+                    addFormError("Internal error (pool name could not resolve - " + poolName + ")");
+                    index++;
+                    continue;
+                }
+                in = _context.tunnelManager().getInboundSettings(client);
+                out = _context.tunnelManager().getOutboundSettings(client);
+            }
+            
+            if ( (in == null) || (out == null) ) {
+                addFormError("Internal error (pool settings cound not be fuond for " + poolName + ")");
+                index++;
+                continue;
+            }
+            
+            in.setLength(getInt(_settings.get(index + ".depthInbound")));
+            out.setLength(getInt(_settings.get(index + ".depthOutbound")));
+            in.setLengthVariance(getInt(_settings.get(index + ".varianceInbound")));
+            out.setLengthVariance(getInt(_settings.get(index + ".varianceOutbound")));
+            in.setQuantity(getInt(_settings.get(index + ".quantityInbound")));
+            out.setQuantity(getInt(_settings.get(index + ".quantityOutbound")));
+            in.setBackupQuantity(getInt(_settings.get(index + ".backupInbound")));
+            out.setBackupQuantity(getInt(_settings.get(index + ".backupOutbound")));
+            
+            if ("exploratory".equals(poolName)) {
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_INBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_LENGTH, in.getLength()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_OUTBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_LENGTH, out.getLength()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_INBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_LENGTH_VARIANCE, in.getLengthVariance()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_OUTBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_LENGTH_VARIANCE, out.getLengthVariance()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_INBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_QUANTITY, in.getQuantity()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_OUTBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_QUANTITY, out.getQuantity()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_INBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_BACKUP_QUANTITY, in.getBackupQuantity()+"");
+                _context.router().setConfigSetting(TunnelPoolSettings.PREFIX_OUTBOUND_EXPLORATORY + 
+                                                   TunnelPoolSettings.PROP_BACKUP_QUANTITY, out.getBackupQuantity()+"");
+            }
+            
+            if ("exploratory".equals(poolName)) {
+                if (_log.shouldLog(Log.DEBUG)) {
+                    _log.debug("Inbound exploratory settings: " + in);
+                    _log.debug("Outbound exploratory settings: " + out);
+                }
+                _context.tunnelManager().setInboundSettings(in);
+                _context.tunnelManager().setOutboundSettings(out);
+            } else {
+                if (_log.shouldLog(Log.DEBUG)) {
+                    _log.debug("Inbound settings for " + client.toBase64() + ": " + in);
+                    _log.debug("Outbound settings for " + client.toBase64() + ": " + out);
+                }
+                _context.tunnelManager().setInboundSettings(client, in);
+                _context.tunnelManager().setOutboundSettings(client, out);
+            }
+            
+            updated++;
+            saveRequired = true;
+            index++;
+        }
+        
+        if (updated > 0)
+            addFormNotice("Updated settings for " + updated + " pools");
+        
+        if (saveRequired) {
+            boolean saved = _context.router().saveConfig();
+            if (saved) 
+                addFormNotice("Configuration saved successfully");
+            else
+                addFormNotice("Error saving the configuration (applied but not saved) - please see the error logs");
+        }
+    }
+    private static final int getInt(Object val) { 
+        if (val == null) return 0;
+        String str = null;
+        if (val instanceof String)
+            str = (String)val;
+        else 
+            str = ((String[])val)[0];
+            
+        if (str.trim().length() <= 0) return 0;
+        try { return Integer.parseInt(str); } catch (NumberFormatException nfe) { return 0; }
+    }
+}
---
+++ apps/routerconsole/java/src/net/i2p/router/web/ConfigTunnelsHelper.java
@@ -0,0 +1,251 @@
+package net.i2p.router.web;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import net.i2p.util.Log;
+
+import net.i2p.data.Destination;
+import net.i2p.router.RouterContext;
+import net.i2p.router.TunnelPoolSettings;
+
+public class ConfigTunnelsHelper {
+    private RouterContext _context;
+    /**
+     * Configure this bean to query a particular router context
+     *
+     * @param contextId begging few characters of the routerHash, or null to pick
+     *                  the first one we come across.
+     */
+    public void setContextId(String contextId) {
+        try {
+            _context = ContextHelper.getContext(contextId);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public ConfigTunnelsHelper() {}
+    
+    
+    public String getForm() {
+        StringBuffer buf = new StringBuffer(1024);
+        buf.append("<table border=\"1\">\n");
+        TunnelPoolSettings exploratoryIn = _context.tunnelManager().getInboundSettings();
+        TunnelPoolSettings exploratoryOut = _context.tunnelManager().getOutboundSettings();
+        
+        buf.append("<input type=\"hidden\" name=\"pool.0\" value=\"exploratory\" >");
+        renderForm(buf, 0, "exploratory", "Exploratory tunnels", exploratoryIn, exploratoryOut);
+        
+        int cur = 1;
+        Set clients = _context.clientManager().listClients();
+        for (Iterator iter = clients.iterator(); iter.hasNext(); ) {
+            Destination dest = (Destination)iter.next();
+            TunnelPoolSettings in = _context.tunnelManager().getInboundSettings(dest.calculateHash());
+            TunnelPoolSettings out = _context.tunnelManager().getOutboundSettings(dest.calculateHash());
+            
+            String name = (in != null ? in.getDestinationNickname() : null);
+            if (name == null)
+                name = (out != null ? out.getDestinationNickname() : null);
+            if (name == null)
+                name = dest.calculateHash().toBase64().substring(0,6);
+        
+            String prefix = dest.calculateHash().toBase64().substring(0,4);
+            buf.append("<input type=\"hidden\" name=\"pool.").append(cur).append("\" value=\"");
+            buf.append(dest.calculateHash().toBase64()).append("\" >");    
+            renderForm(buf, cur, prefix, "Client tunnels for " + name, in, out);
+            cur++;
+        }
+        
+        buf.append("</table>\n");
+        return buf.toString();
+    }
+
+    private void renderForm(StringBuffer buf, int index, String prefix, String name, TunnelPoolSettings in, TunnelPoolSettings out) {
+
+        buf.append("<tr><td colspan=\"3\"><b><a name=\"").append(prefix).append("\">");
+        buf.append(name).append("</a></b></td></tr>\n");
+        buf.append("<tr><td></td><td><b>Inbound</b></td><td><b>Outbound</b></td></tr>\n");
+        
+        // tunnel depth
+        buf.append("<tr><td>Depth</td>\n");
+        buf.append("<td><select name=\"").append(index).append(".depthInbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (in.getLength() <= 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 hops</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (in.getLength() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 hop</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (in.getLength() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 hops</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (in.getLength() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 hops</option>\n");
+        if (in.getLength() > 3)
+            buf.append("<option value=\"").append(in.getLength()).append("\">").append(in.getLength()).append(" hops</option>\n");
+        buf.append("</td>\n");
+
+        buf.append("<td><select name=\"").append(index).append(".depthOutbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (out.getLength() <= 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 hops</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (out.getLength() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 hop</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (out.getLength() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 hops</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (out.getLength() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 hops</option>\n");
+        if (out.getLength() > 3)
+            buf.append("<option value=\"").append(out.getLength()).append("\">").append(out.getLength()).append(" hops</option>\n");
+        buf.append("</td>\n");
+        buf.append("</tr>\n");
+
+        // tunnel depth variance
+        buf.append("<tr><td>Variance</td>\n");
+        buf.append("<td><select name=\"").append(index).append(".varianceInbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (in.getLengthVariance() == 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 hops</option>\n");
+        buf.append("<option value=\"-1\" ");
+        if (in.getLengthVariance() == -1) buf.append(" selected=\"true\" ");
+        buf.append(">+/- 0-1 hops</option>\n");
+        buf.append("<option value=\"-2\" ");
+        if (in.getLengthVariance() == -2) buf.append(" selected=\"true\" ");
+        buf.append(">+/- 0-2 hops</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (in.getLengthVariance() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">+ 0-1 hops</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (in.getLengthVariance() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">+ 0-2 hops</option>\n");
+        if (in.getLengthVariance() < -2)
+            buf.append("<option value=\"").append(in.getLengthVariance()).append("\">+/- 0-").append(in.getLengthVariance()).append(" hops</option>\n");
+        if (in.getLengthVariance() > 2)
+            buf.append("<option value=\"").append(in.getLengthVariance()).append("\">+ 0-").append(in.getLengthVariance()).append(" hops</option>\n");
+        buf.append("</td>\n");
+
+        buf.append("<td><select name=\"").append(index).append(".varianceOutbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (out.getLengthVariance() == 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 hops</option>\n");
+        buf.append("<option value=\"-1\" ");
+        if (out.getLengthVariance() == -1) buf.append(" selected=\"true\" ");
+        buf.append(">+/- 0-1 hops</option>\n");
+        buf.append("<option value=\"-2\" ");
+        if (out.getLengthVariance() == -2) buf.append(" selected=\"true\" ");
+        buf.append(">+/- 0-2 hops</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (out.getLengthVariance() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">+ 0-1 hops</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (out.getLengthVariance() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">+ 0-2 hops</option>\n");
+        if (out.getLengthVariance() < -2)
+            buf.append("<option value=\"").append(out.getLengthVariance()).append("\">+/- 0-").append(out.getLengthVariance()).append(" hops</option>\n");
+        if (out.getLengthVariance() > 2)
+            buf.append("<option value=\"").append(out.getLengthVariance()).append("\">+ 0-").append(out.getLengthVariance()).append(" hops</option>\n");
+        buf.append("</td>\n");
+
+        // tunnel quantity
+        buf.append("<tr><td>Quantity</td>\n");
+        buf.append("<td><select name=\"").append(index).append(".quantityInbound\">\n");
+        buf.append("<option value=\"1\" ");
+        if (in.getQuantity() <= 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 tunnel</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (in.getQuantity() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 tunnels</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (in.getQuantity() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 tunnels</option>\n");
+        if (in.getQuantity() > 3)
+            buf.append("<option value=\"").append(in.getQuantity()).append("\">").append(in.getQuantity()).append(" tunnels</option>\n");
+        buf.append("</td>\n");
+
+        buf.append("<td><select name=\"").append(index).append(".quantityOutbound\">\n");
+        buf.append("<option value=\"1\" ");
+        if (out.getQuantity() <= 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 tunnel</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (out.getQuantity() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 tunnels</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (out.getQuantity() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 tunnels</option>\n");
+        if (out.getQuantity() > 3)
+            buf.append("<option value=\"").append(out.getQuantity()).append("\">").append(out.getQuantity()).append(" tunnels</option>\n");
+        buf.append("</td>\n");
+        buf.append("</tr>\n");
+
+        // tunnel backup quantity
+        buf.append("<tr><td>Backup quantity</td>\n");
+        buf.append("<td><select name=\"").append(index).append(".backupInbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (in.getBackupQuantity() <= 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 tunnels</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (in.getBackupQuantity() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 tunnel</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (in.getBackupQuantity() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 tunnels</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (in.getBackupQuantity() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 tunnels</option>\n");
+        if (in.getBackupQuantity() > 3)
+            buf.append("<option value=\"").append(in.getBackupQuantity()).append("\">").append(in.getBackupQuantity()).append(" tunnels</option>\n");
+        buf.append("</td>\n");
+
+        buf.append("<td><select name=\"").append(index).append(".backupOutbound\">\n");
+        buf.append("<option value=\"0\" ");
+        if (out.getBackupQuantity() <= 0) buf.append(" selected=\"true\" ");
+        buf.append(">0 tunnel</option>\n");
+        buf.append("<option value=\"1\" ");
+        if (out.getBackupQuantity() == 1) buf.append(" selected=\"true\" ");
+        buf.append(">1 tunnel</option>\n");
+        buf.append("<option value=\"2\" ");
+        if (out.getBackupQuantity() == 2) buf.append(" selected=\"true\" ");
+        buf.append(">2 tunnels</option>\n");
+        buf.append("<option value=\"3\" ");
+        if (out.getBackupQuantity() == 3) buf.append(" selected=\"true\" ");
+        buf.append(">3 tunnels</option>\n");
+        if (out.getBackupQuantity() > 3)
+            buf.append("<option value=\"").append(out.getBackupQuantity()).append("\">").append(out.getBackupQuantity()).append(" tunnels</option>\n");
+        buf.append("</td>\n");
+        buf.append("</tr>\n");
+
+        // custom options
+        buf.append("<tr><td>Inbound options:</td>\n");
+        buf.append("<td colspan=\"2\"><input name=\"").append(index);
+        buf.append(".inboundOptions\" type=\"text\" size=\"40\" ");
+        buf.append("value=\"");
+        Properties props = in.getUnknownOptions();
+        for (Iterator iter = props.keySet().iterator(); iter.hasNext(); ) {
+            String prop = (String)iter.next();
+            String val = (String)props.getProperty(prop);
+            buf.append(prop).append("=").append(val).append(" ");
+        }
+        buf.append("\"/></td></tr>\n");
+        buf.append("<tr><td>Outbound options:</td>\n");
+        buf.append("<td colspan=\"2\"><input name=\"").append(index);
+        buf.append(".outboundOptions\" type=\"text\" size=\"40\" ");
+        buf.append("value=\"");
+        props = in.getUnknownOptions();
+        for (Iterator iter = props.keySet().iterator(); iter.hasNext(); ) {
+            String prop = (String)iter.next();
+            String val = (String)props.getProperty(prop);
+            buf.append(prop).append("=").append(val).append(" ");
+        }
+        buf.append("\"/></td></tr>\n");
+        buf.append("<tr><td colspan=\"3\"><hr /></td></tr>\n");
+    }
+}
