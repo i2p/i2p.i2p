2e7eac02edb51bc904c6072208a4c53e5a5a13d4
jrandom <jrandom>
2005-02-27 00:03:42 +0000

    2005-02-26  jrandom     * Force 1.3-isms on the precompiled jsps too (thanks laberhost)

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.152 $ $Date: 2005/02/24 18:53:36 $";
+    public final static String ID = "$Revision: 1.153 $ $Date: 2005/02/26 14:16:47 $";
     public final static String VERSION = "0.5.0.1";
-    public final static long BUILD = 4;
+    public final static long BUILD = 5;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION);
         System.out.println("Router ID: " + RouterVersion.ID);
