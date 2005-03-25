85b345052585836770d0ef4eb087117f567d4d93
jrandom <jrandom>
2005-03-25 04:07:05 +0000

    2005-03-25  jrandom     * Fixed up building dependencies for the routerconsole on some more       aggressive compilers (thanks polecat!)

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.172 $ $Date: 2005/03/23 22:18:15 $";
+    public final static String ID = "$Revision: 1.173 $ $Date: 2005/03/24 02:29:27 $";
     public final static String VERSION = "0.5.0.4";
-    public final static long BUILD = 0;
+    public final static long BUILD = 1;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION);
         System.out.println("Router ID: " + RouterVersion.ID);
