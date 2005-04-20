df926fb60db52c1884c5a236e33f84d084c1ebaa
jrandom <jrandom>
2005-04-20 20:14:17 +0000

    * 2005-04-20  0.5.0.7 released

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.187 $ $Date: 2005/04/17 21:07:58 $";
-    public final static String VERSION = "0.5.0.6";
-    public final static long BUILD = 5;
+    public final static String ID = "$Revision: 1.188 $ $Date: 2005/04/20 14:15:25 $";
+    public final static String VERSION = "0.5.0.7";
+    public final static long BUILD = 0;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION);
         System.out.println("Router ID: " + RouterVersion.ID);
