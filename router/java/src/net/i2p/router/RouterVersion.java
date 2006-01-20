cd235e5902c50d12669617267cc994bace97856f
complication <complication>
2006-01-20 04:40:24 +0000

    2006-01-19  Complication     * Explain better where eepsite's destkey can be found

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.335 $ $Date: 2006/01/17 17:56:16 $";
+    public final static String ID = "$Revision: 1.336 $ $Date: 2006/01/18 01:38:49 $";
     public final static String VERSION = "0.6.1.9";
-    public final static long BUILD = 4;
+    public final static long BUILD = 5;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION + "-" + BUILD);
         System.out.println("Router ID: " + RouterVersion.ID);
