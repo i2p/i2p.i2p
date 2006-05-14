2ad715c6681b54cdf64f4fc438b4a465dea7705e
complication <complication>
2006-05-14 05:11:36 +0000

    2006-05-13  Complication     * Update the build number too

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.412 $ $Date: 2006/05/09 16:17:22 $";
+    public final static String ID = "$Revision: 1.413 $ $Date: 2006/05/11 22:31:47 $";
     public final static String VERSION = "0.6.1.18";
-    public final static long BUILD = 1;
+    public final static long BUILD = 2;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION + "-" + BUILD);
         System.out.println("Router ID: " + RouterVersion.ID);
