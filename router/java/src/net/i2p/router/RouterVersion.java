d705e0ad048d0134f3e6e5bce0d07cb02ba973e9
zzz <zzz>
2007-01-17 05:13:27 +0000

    2007-01-17  zzz     * Add note on Syndie index.html steering people to new Syndie

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.473 $ $Date: 2007-01-15 23:47:58 $";
+    public final static String ID = "$Revision: 1.474 $ $Date: 2007-01-16 01:20:23 $";
     public final static String VERSION = "0.6.1.26";
-    public final static long BUILD = 7;
+    public final static long BUILD = 8;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION + "-" + BUILD);
         System.out.println("Router ID: " + RouterVersion.ID);
