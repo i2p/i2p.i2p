036802d66a54a84c2dfb9f036cf99442685f9636
zzz <zzz>
2007-03-18 21:57:01 +0000

    2007-03-18  zzz     * i2psnark: Cleanup some handling of saved partial pieces     * i2psnark: Put bit counting in Bitfield.java for efficiency     * i2psnark: Save torrent completion state in i2psnark.config

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -15,9 +15,9 @@ import net.i2p.CoreVersion;
  *
  */
 public class RouterVersion {
-    public final static String ID = "$Revision: 1.497 $ $Date: 2007-03-15 13:26:06 $";
+    public final static String ID = "$Revision: 1.498 $ $Date: 2007-03-17 20:48:13 $";
     public final static String VERSION = "0.6.1.28";
-    public final static long BUILD = 0;
+    public final static long BUILD = 1;
     public static void main(String args[]) {
         System.out.println("I2P Router version: " + VERSION + "-" + BUILD);
         System.out.println("Router ID: " + RouterVersion.ID);
