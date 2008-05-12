5be7ea1fc587fe53db912cd83b789d808e59e310
zzz <zzz@mail.i2p>
2008-05-12 13:47:15 +0000

    * libjbigi:       - Add documentation on dynamic build option       - Add two speed tests to the build script       - Clean up the build script, make it easier to build dynamic

--- core/c/jbigi/build.sh
+++ core/c/jbigi/build.sh
@@ -1,22 +1,67 @@
 #/bin/sh
+#
+#  Build the jbigi library for i2p
+#
+#  To build a static library:
+#     download gmp-4.2.2.tar.bz2 to this directory
+#       (if a different version, change the VER= line below)
+#     build.sh
+#
+#  To build a dynamic library (you must have a libgmp.so somewhere in your system)
+#     build.sh dynamic
+#
+#  The resulting library is lib/libjbigi.so
+#
 
-VER=4.2.2
-echo "Building the jbigi library with GMP Version $VER"
-
-echo "Extracting GMP..."
-tar -xjf gmp-$VER.tar.bz2
-echo "Building..."
 mkdir -p lib/
 mkdir -p bin/local
+VER=4.2.2
+
+if [ "$1" != "dynamic" -a ! -d gmp-$VER ]
+then
+	TAR=gmp-$VER.tar.bz2
+        if [ ! -f $TAR ]
+        then
+		echo "GMP tarball $TAR not found. You must download it from http://gmplib.org/"
+		exit 1
+        fi
+
+	echo "Building the jbigi library with GMP Version $VER"
+
+	echo "Extracting GMP..."
+	tar -xjf gmp-$VER.tar.bz2
+fi
+
 cd bin/local
-case `uname -sr` in
-Darwin*)
-# --with-pic is required for static linking
-../../gmp-$VER/configure --with-pic;;
-*)
-../../gmp-$VER/configure;;
-esac
-make
-sh ../../build_jbigi.sh static
+
+echo "Building..."
+if [ "$1" != "dynamic" ]
+then
+	case `uname -sr` in
+		Darwin*)
+			# --with-pic is required for static linking
+			../../gmp-$VER/configure --with-pic;;
+		*)
+			../../gmp-$VER/configure;;
+	esac
+	make
+	sh ../../build_jbigi.sh static
+else
+	sh ../../build_jbigi.sh dynamic
+fi
+
 cp *jbigi???* ../../lib/
+echo 'Library copied to lib/'
 cd ../..
+
+I2P=~/i2p
+if [ ! -f $I2P/lib/i2p.jar ]
+then
+	echo "I2P installation not found in $I2P - correct \$I2P definition in script to run speed test"
+	exit 1
+fi
+echo 'Running test with standard I2P installation...'
+java -cp $I2P/lib/i2p.jar:$I2P/lib/jbigi.jar net.i2p.util.NativeBigInteger
+echo
+echo 'Running test with new libjbigi...'
+java -Djava.library.path=lib/ -cp $I2P/lib/i2p.jar:$I2P/lib/jbigi.jar net.i2p.util.NativeBigInteger
