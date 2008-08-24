bf517411347bde3fc40b4e06390b5035cc54c376
complication <complication@mail.i2p>
2008-08-24 10:28:57 +0000

    * Update versions, package release

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -14,8 +14,8 @@ package net.i2p;
  *
  */
 public class CoreVersion {
-    public final static String ID = "$Revision: 1.72 $ $Date: 2008-06-07 23:00:00 $";
-    public final static String VERSION = "0.6.2";
+    public final static String ID = "$Revision: 1.72 $ $Date: 2008-08-24 12:00:00 $";
+    public final static String VERSION = "0.6.3";
 
     public static void main(String args[]) {
         System.out.println("I2P Core version: " + VERSION);
