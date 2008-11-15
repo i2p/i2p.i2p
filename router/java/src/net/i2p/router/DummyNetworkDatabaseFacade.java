b1a9dcf77ee657fe7cc3deabe56d0ed26c8e54e3
zzz <zzz@mail.i2p>
2008-11-15 19:09:56 +0000

    move dummy to his own file to help the build dependencies

---
+++ router/java/src/net/i2p/router/DummyNetworkDatabaseFacade.java
@@ -0,0 +1,64 @@
+package net.i2p.router;
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by jrandom in 2003 and released into the public domain 
+ * with no warranty of any kind, either expressed or implied.  
+ * It probably won't make your computer catch on fire, or eat 
+ * your children, but it might.  Use at your own risk.
+ *
+ */
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.i2p.data.Hash;
+import net.i2p.data.LeaseSet;
+import net.i2p.data.RouterInfo;
+
+class DummyNetworkDatabaseFacade extends NetworkDatabaseFacade {
+    private Map _routers;
+    private RouterContext _context;
+    
+    public DummyNetworkDatabaseFacade(RouterContext ctx) {
+        _routers = Collections.synchronizedMap(new HashMap());
+        _context = ctx;
+    }
+
+    public void restart() {}
+    public void shutdown() {}
+    public void startup() {
+        RouterInfo info = _context.router().getRouterInfo();
+        _routers.put(info.getIdentity().getHash(), info);
+    }
+    
+    public void lookupLeaseSet(Hash key, Job onFindJob, Job onFailedLookupJob, long timeoutMs) {}
+    public LeaseSet lookupLeaseSetLocally(Hash key) { return null; }
+    public void lookupRouterInfo(Hash key, Job onFindJob, Job onFailedLookupJob, long timeoutMs) {
+        RouterInfo info = lookupRouterInfoLocally(key);
+        if (info == null) 
+            _context.jobQueue().addJob(onFailedLookupJob);
+        else
+            _context.jobQueue().addJob(onFindJob);
+    }
+    public RouterInfo lookupRouterInfoLocally(Hash key) { return (RouterInfo)_routers.get(key); }
+    public void publish(LeaseSet localLeaseSet) {}
+    public void publish(RouterInfo localRouterInfo) {}
+    public LeaseSet store(Hash key, LeaseSet leaseSet) { return leaseSet; }
+    public RouterInfo store(Hash key, RouterInfo routerInfo) {
+        RouterInfo rv = (RouterInfo)_routers.put(key, routerInfo);
+        return rv;
+    }
+    public void unpublish(LeaseSet localLeaseSet) {}
+    public void fail(Hash dbEntry) {
+        _routers.remove(dbEntry);
+    }
+    
+    public Set findNearestRouters(Hash key, int maxNumRouters, Set peersToIgnore) { return new HashSet(_routers.values()); }
+
+    public void renderStatusHTML(Writer out) throws IOException {}
+}
