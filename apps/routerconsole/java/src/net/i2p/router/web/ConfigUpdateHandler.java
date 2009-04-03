8de560981709863847cfc90755de697a3d56dc40
zzz <zzz@mail.i2p>
2009-04-03 21:42:48 +0000

    * Update:       - Change default to "Download and verify"       - Change news fetch default to 24h (was 12h)

--- apps/routerconsole/java/src/net/i2p/router/web/ConfigUpdateHandler.java
+++ apps/routerconsole/java/src/net/i2p/router/web/ConfigUpdateHandler.java
@@ -21,9 +21,9 @@ public class ConfigUpdateHandler extends FormHandler {
 //  public static final String DEFAULT_NEWS_URL = "http://dev.i2p.net/cgi-bin/cvsweb.cgi/i2p/news.xml?rev=HEAD";
     public static final String DEFAULT_NEWS_URL = "http://complication.i2p/news.xml";
     public static final String PROP_REFRESH_FREQUENCY = "router.newsRefreshFrequency";
-    public static final String DEFAULT_REFRESH_FREQUENCY = 12*60*60*1000 + "";
+    public static final String DEFAULT_REFRESH_FREQUENCY = 24*60*60*1000 + "";
     public static final String PROP_UPDATE_POLICY = "router.updatePolicy";
-    public static final String DEFAULT_UPDATE_POLICY = "notify";
+    public static final String DEFAULT_UPDATE_POLICY = "download";
     public static final String PROP_SHOULD_PROXY = "router.updateThroughProxy";
     public static final String DEFAULT_SHOULD_PROXY = Boolean.TRUE.toString();
     public static final String PROP_PROXY_HOST = "router.updateProxyHost";
