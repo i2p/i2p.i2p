66a292a9c6c8a083794db9a839a50a598cd407ce
sponge <sponge@mail.i2p>
2009-05-12 21:10:42 +0000

    2009-05-12 sponge     * BOB clean up, change println's to _log.warn, bump BOB version     * I2PSessionMuxedImpl.java changes as per zzz, and they test OK for me.

--- apps/BOB/src/net/i2p/BOB/DoCMDS.java
+++ apps/BOB/src/net/i2p/BOB/DoCMDS.java
@@ -46,7 +46,7 @@ public class DoCMDS implements Runnable {
 
 	// FIX ME
 	// I need a better way to do versioning, but this will do for now.
-	public static final String BMAJ = "00",  BMIN = "00",  BREV = "05",  BEXT = "";
+	public static final String BMAJ = "00",  BMIN = "00",  BREV = "06",  BEXT = "";
 	public static final String BOBversion = BMAJ + "." + BMIN + "." + BREV + BEXT;
 	private Socket server;
 	private Properties props;
