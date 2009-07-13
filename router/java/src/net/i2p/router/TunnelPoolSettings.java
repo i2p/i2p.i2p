de7dc2c1f79a653d532f1ce213518560c944b628
zzz <zzz@mail.i2p>
2009-07-13 02:54:47 +0000

    * Tunnels: Change the default variance from 1 to 0.       Under the one-packet-enough theory, and the fact that most       tunnels in a x+1 pool are of length x, variable lengths       don't really help that much. Also, a default of 1 led       to all sorts of problems with iMule/SAM, who was not       setting the variance properties.       This will affect exploratory tunnels for new users,       and those that have never saved a change on configtunnels.jsp,       and iMule users 1.4.5 and earlier.

--- router/java/src/net/i2p/router/TunnelPoolSettings.java
+++ router/java/src/net/i2p/router/TunnelPoolSettings.java
@@ -49,7 +49,7 @@ public class TunnelPoolSettings {
     // public static final int     DEFAULT_REBUILD_PERIOD = 60*1000;
     public static final int     DEFAULT_DURATION = 10*60*1000;
     public static final int     DEFAULT_LENGTH = 2;
-    public static final int     DEFAULT_LENGTH_VARIANCE = 1;
+    public static final int     DEFAULT_LENGTH_VARIANCE = 0;
     public static final boolean DEFAULT_ALLOW_ZERO_HOP = true;
     public static final int     DEFAULT_IP_RESTRICTION = 2;    // class B (/16)
     
