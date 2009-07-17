70a17ecfaffc213987469264c2f05b4b73ed07da
zzz <zzz@mail.i2p>
2009-07-17 16:53:53 +0000

    provide a refresh link since the refresh button brings up a POST dialog

--- apps/routerconsole/java/src/net/i2p/router/web/ConfigUIHandler.java
+++ apps/routerconsole/java/src/net/i2p/router/web/ConfigUIHandler.java
@@ -19,12 +19,12 @@ public class ConfigUIHandler extends FormHandler {
     private void saveChanges() {
         if (_config == null)
             return;
-        if (_config.equals("default"))
+        if (_config.equals("default")) // obsolete
             _context.router().removeConfigSetting(CSSHelper.PROP_THEME_NAME);
         else
             _context.router().setConfigSetting(CSSHelper.PROP_THEME_NAME, _config);
         if (_context.router().saveConfig()) 
-            addFormNotice("Theme change successfully saved (refresh page to view)");
+            addFormNotice("Theme change successfully saved (<a href=\"configui.jsp\">refresh page to view</a>)");
         else
             addFormNotice("Error saving the configuration (applied but not saved) - please see the error logs");
     }
