060f4e6632875b6b9cbe4aa367fea5a6bfd1be02
dev <dev@welterde.de>
2009-11-16 19:44:54 +0000

    disapproval of revision 'd07b92fb678f4dc87b62eaaeab002820b25583b6'

--- apps/i2pbote/src/i2p/bote/network/kademlia/KademliaException.java
+++
@@ -1,9 +0,0 @@
-package i2p.bote.network.kademlia;
-
-public class KademliaException extends Exception {
-    private static final long serialVersionUID = -1286128818859245017L;
-
-    public KademliaException(String message) {
-        super(message);
-    }
-}
--- apps/i2pbote/src/i2p/bote/service/StreamHandler.java
+++
@@ -1,8 +0,0 @@
-package i2p.bote.service;
-
-import java.util.EventListener;
-
-public interface StreamHandler extends EventListener {
-
-    void streamReceived(StreamReceivedEvent event);
-}
