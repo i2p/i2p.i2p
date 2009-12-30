afd54fc212ddc2b339b6a0c50b571d9ba2ae7919
zzz <zzz@mail.i2p>
2009-12-30 22:25:21 +0000

    logs.jsp tweak

--- apps/routerconsole/java/src/net/i2p/router/web/LogsHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/LogsHelper.java
@@ -10,7 +10,7 @@ public class LogsHelper extends HelperBase {
     
     public String getLogs() {
         String str = formatMessages(_context.logManager().getBuffer().getMostRecentMessages());
-        return "Location: <b><code>" + _context.logManager().currentFile() + "</code></b><br><br>" + str;
+        return _("File location") + ": <b><code>" + _context.logManager().currentFile() + "</code></b><br><br>" + str;
     }
     
     public String getCriticalLogs() {
@@ -34,7 +34,7 @@ public class LogsHelper extends HelperBase {
             return "";
         else {
             str = str.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-            return _("File location") + ":<b><code> " + f.getAbsolutePath() + "</code></b> <pre>" + str + "</pre>";
+            return _("File location") + ": <b><code>" + f.getAbsolutePath() + "</code></b> <pre>" + str + "</pre>";
         }
     }
     
