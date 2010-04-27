c212eacf1955f9c517b16278e840f20720b8d31e
walking <walking@mail.i2p>
2010-04-27 15:01:03 +0000

    - add new target: pkg-portable-win32 (must run buildSmall first) - add configs/win batchfiles to installer/resources/portable
    * currently only pkg-portable-win32 on win32 available
    need linuxers to write target preppkg-portable-nix/pkg-portable-linux
    and enable pkg-portable-win32 on linux (i doubt anyone need it ?)
    shell scripts should goto installer/resources/portable/configs/linux/

---
+++ installer/resources/portable/win32/ClickMe2Unpack.bat
@@ -0,0 +1,8 @@
+@echo off
+path %path%;c:\program Files\Java\jre6\bin;
+echo ==========  Unpacking .jar libs, plz wait ...  ============
+for %%i in (lib\*.pack) do unpack200 -r %%i lib\%%~ni.jar
+ren batch1.na StartI2P.bat
+ren batch2.na EepGet.bat
+ren i2psvc.ex_ i2psvc.exe
+del ClickMe2UnPack.bat
