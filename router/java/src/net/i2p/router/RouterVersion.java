f054663b1791365e4b45e2efbe60e6c9da2c61a0
z3d <z3d@mail.i2p>
2010-12-05 23:50:12 +0000

    -32rc

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -18,7 +18,7 @@ public class RouterVersion {
     /** deprecated */
     public final static String ID = "Monotone";
     public final static String VERSION = CoreVersion.VERSION;
-    public final static long BUILD = 31;
+    public final static long BUILD = 32;
 
     /** for example "-test" */
     public final static String EXTRA = "-rc";
