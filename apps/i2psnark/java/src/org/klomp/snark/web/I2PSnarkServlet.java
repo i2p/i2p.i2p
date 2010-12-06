a1698b8eab1c1b258ec69cafcc6aa33ba15f3b00
z3d <z3d@mail.i2p>
2010-12-06 17:51:10 +0000

    Light, ubergine & snark changes.

--- apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
+++ apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
@@ -352,14 +352,24 @@ public class I2PSnarkServlet extends Default {
             out.write("&nbsp;");
 //            out.write(ngettext("1 connected peer", "{0} connected peers", (int) stats[4]));
 
-            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_peers.png\">");
+            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_peers.png\"");
+            out.write(" title=\"");
+            out.write(ngettext("1 connected peer", "{0} connected peers", (int) stats[4]));
+            out.write("\"> ");
             out.write(ngettext("1", "{0}", (int) stats[4]));
             out.write("&nbsp;&nbsp;");
 //            out.write(ngettext("1 torrent", "{0} torrents", snarks.size()));
-            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_snarks.png\">");
+            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_snarks.png\"");
+            out.write(" title=\"");
+            out.write(ngettext("1 torrent loaded", "{0} torrents loaded", snarks.size()));
+            out.write("\"> ");
             out.write(ngettext("1", "{0}", snarks.size()));
             out.write("&nbsp;&nbsp;");
-            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_loaded.png\">");
+            out.write("<img border=\"0\" src=\"/themes/snark/" + _manager.getTheme() + "/images/head_loaded.png\"");
+            out.write(" title=\"");
+            out.write(_("Total download size (pre-allocated): "));
+            out.write(DataHelper.formatSize2(stats[5]) + "B");
+            out.write("\"> ");
             out.write(DataHelper.formatSize2(stats[5]) + "B");
             out.write("</th>\n" +
                       "    <th>&nbsp;</th>\n" +
