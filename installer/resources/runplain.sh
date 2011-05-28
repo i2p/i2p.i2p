33b9a5c2ce6bd14c7a146a8c465f2a74ac2fd6a7
kytv <kytv@mail.i2p>
2011-05-28 20:14:36 +0000

    Make runplain shell agnostic
    Braces were put around variable names so that router will run in kFreeBSD.
    runplain.sh should now be shell agnostic (it runs in dash. it even runs in
    *posh*, a ridiculously picky shell).

--- installer/resources/runplain.sh
+++ installer/resources/runplain.sh
@@ -14,9 +14,9 @@
 I2P="%INSTALL_PATH"
 I2PTEMP="%SYSTEM_java_io_tmpdir"
 
-export CP="$I2P" ; for j in "$I2P/lib/*"  ; do export CP="$CP:$j" ; done;
+export CP="${I2P}" ; for j in "${I2P}/lib/*"  ; do export CP="${CP}:${j}" ; done;
 JAVA=java
 
-JAVAOPTS="-Djava.library.path=$I2P:$I2P/lib -Di2p.dir.base=$I2P -DloggerFilenameOverride=logs/log-router-@.txt"
-nohup $JAVA -cp "$CP" $JAVAOPTS net.i2p.router.RouterLaunch > /dev/null 2>&1 &
-echo $! > "$I2PTEMP/router.pid"
+JAVAOPTS="-Djava.library.path=$ {I2P}:${I2P}/lib -Di2p.dir.base=${I2P} -DloggerFilenameOverride=logs/log-router-@.txt"
+nohup ${JAVA} -cp "${CP}" ${JAVAOPTS} net.i2p.router.RouterLaunch > /dev/null 2>&1 &
+echo $! > "${I2PTEMP}/router.pid"
