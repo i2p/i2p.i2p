8a56531c90c37f90bdbf53089b94a5129a034045
kytv <kytv@mail.i2p>
2011-06-01 14:42:18 +0000

    Don't prefer IPv4 but add the option commented out to wrapper.config. Make the option configurable in runplain.sh

--- installer/resources/runplain.sh
+++ installer/resources/runplain.sh
@@ -14,9 +14,13 @@
 I2P="%INSTALL_PATH"
 I2PTEMP="%SYSTEM_java_io_tmpdir"
 
+# Having IPv6 enabled can cause problems with certain configurations. Changing the
+# next value to true may help. 
+PREFERv4="false"
+
 export CP="${I2P}" ; for j in "${I2P}/lib/*"  ; do export CP="${CP}:${j}" ; done;
 JAVA=java
 
-JAVAOPTS="-Djava.net.preferIPv4Stack=true -Djava.library.path=${I2P}:${I2P}/lib -Di2p.dir.base=${I2P} -DloggerFilenameOverride=logs/log-router-@.txt"
+JAVAOPTS="-Djava.net.preferIPv4Stack=${PREFERv4} -Djava.library.path=${I2P}:${I2P}/lib -Di2p.dir.base=${I2P} -DloggerFilenameOverride=logs/log-router-@.txt"
 nohup ${JAVA} -cp "${CP}" ${JAVAOPTS} net.i2p.router.RouterLaunch > /dev/null 2>&1 &
 echo $! > "${I2PTEMP}/router.pid"
