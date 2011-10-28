042da4d921864ec4964ce2b2f04944ab88070b18
zzz <zzz@mail.i2p>
2011-10-28 01:27:11 +0000

    max conns

--- router/java/src/net/i2p/router/transport/TransportImpl.java
+++ router/java/src/net/i2p/router/transport/TransportImpl.java
@@ -118,7 +118,7 @@ public abstract class TransportImpl implements Transport {
         // increase limit for SSU, for now
         if (style.equals("udp"))
             //def = def * 3 / 2;
-            def *= 2;
+            def *= 3;
         return _context.getProperty("i2np." + style + ".maxConnections", def);
     }
 
