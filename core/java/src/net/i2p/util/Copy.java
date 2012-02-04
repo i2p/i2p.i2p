c0e0994bb31fa887cb48a118a9006e3dcc8f7921
zzz <zzz@mail.i2p>
2012-02-04 13:02:15 +0000

    deprecate util classes used only by installer

--- core/java/src/net/i2p/util/Copy.java
+++ core/java/src/net/i2p/util/Copy.java
@@ -3,6 +3,7 @@ package net.i2p.util;
 /**
  * Usage: Copy from to
  *
+ * @deprecated only for use by installer, to be removed from i2p.jar, use FileUtil.copy()
  */
 public class Copy {
     public static void main(String args[]) {
--- core/java/src/net/i2p/util/Delete.java
+++ core/java/src/net/i2p/util/Delete.java
@@ -3,6 +3,7 @@ package net.i2p.util;
 /**
  * Usage: Delete name
  *
+ * @deprecated only for use by installer, to be removed from i2p.jar, use FileUtil.rmdir()
  */
 public class Delete {
     public static void main(String args[]) {
