1d40a88166de41ec49d2f64801cb1262c529ce74
str4d <str4d@mail.i2p>
2012-07-17 20:55:36 +0000

    Fix snark Ajax JS (wrong level of escaping of quotes)

--- apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
+++ apps/i2psnark/java/src/org/klomp/snark/web/I2PSnarkServlet.java
@@ -232,7 +232,7 @@ public class I2PSnarkServlet extends DefaultServlet {
                 //out.write("<meta http-equiv=\"refresh\" content=\"" + delay + ";/i2psnark/" + peerString + "\">\n");
                 out.write("<script src=\"/js/ajax.js\" type=\"text/javascript\"></script>\n" +
                           "<script type=\"text/javascript\">\n"  +
-                          "var failMessage = \"<div class=\"routerdown\"><b>" + _("Router is down") + "<\\/b></div>\";\n" +
+                          "var failMessage = \"<div class=\\\"routerdown\\\"><b>" + _("Router is down") + "<\\/b></div>\";\n" +
                           "function requestAjax1() { ajax(\"/i2psnark/.ajax/xhr1.html" + peerString + "\", \"mainsection\", " + (delay*1000) + "); }\n" +
                           "function initAjax() { setTimeout(requestAjax1, " + (delay*1000) +");  }\n"  +
                           "</script>\n");
