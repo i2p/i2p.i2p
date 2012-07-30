77a19a0b171e2d38b9637a601c43a3451fbb4cf8
zzz <zzz@mail.i2p>
2012-07-30 13:28:56 +0000

    0.9.1

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -16,7 +16,7 @@ package net.i2p;
 public class CoreVersion {
     /** deprecated */
     public final static String ID = "Monotone";
-    public final static String VERSION = "0.9";
+    public final static String VERSION = "0.9.1";
 
     public static void main(String args[]) {
         System.out.println("I2P Core version: " + VERSION);
