4d32eaa036ce13d92151e3b78f71453f77dbbe92
zzz <zzz@mail.i2p>
2012-08-03 11:21:28 +0000

    * JobQueue: Synch fix (ticket #670)

--- router/java/src/net/i2p/router/JobQueue.java
+++ router/java/src/net/i2p/router/JobQueue.java
@@ -211,10 +211,14 @@ public class JobQueue {
     /**
      * Returns <code>true</code> if a given job is waiting or running;
      * <code>false</code> if the job is finished or doesn't exist in the queue.
+     *
+     * Only used by PluginStarter, candidate for deprecation
      */
     public boolean isJobActive(Job job) {
-        if (_readyJobs.contains(job) || _timedJobs.contains(job))
-            return true;
+        synchronized (_jobLock) {
+            if (_readyJobs.contains(job) || _timedJobs.contains(job))
+                return true;
+        }
         for (JobQueueRunner runner: _queueRunners.values())
             if (runner.getCurrentJob() == job)
                 return true;
