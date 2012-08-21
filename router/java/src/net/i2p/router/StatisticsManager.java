fbd8c69eea0561747ef510a91d8ae61319283453
zzz <zzz@mail.i2p>
2012-08-21 19:49:43 +0000

    * NetDB: Decrease stat publish probability

--- router/java/src/net/i2p/router/StatisticsManager.java
+++ router/java/src/net/i2p/router/StatisticsManager.java
@@ -31,7 +31,7 @@ public class StatisticsManager implements Service {
     
     public final static String PROP_PUBLISH_RANKINGS = "router.publishPeerRankings";
     /** enhance anonymity by only including build stats one out of this many times */
-    private static final int RANDOM_INCLUDE_STATS = 4;
+    private static final int RANDOM_INCLUDE_STATS = 8;
 
     private final DecimalFormat _fmt;
     private final DecimalFormat _pct;
