15a47b561281d1575192bc7a7d82c9a3388e9e7b
zzz <zzz@mail.i2p>
2012-10-03 19:17:55 +0000

    import

--- core/java/src/net/i2p/util/Addresses.java
+++ core/java/src/net/i2p/util/Addresses.java
@@ -10,7 +10,6 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
