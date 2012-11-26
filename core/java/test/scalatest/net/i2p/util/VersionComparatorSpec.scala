4266a10ffbad5c04d7ddabdb3ac3b028fbb86205
str4d <str4d@mail.i2p>
2012-11-26 00:29:02 +0000

    Added more tests to VersionComparatorSpec to further cover the implementation

--- core/java/test/scalatest/net/i2p/util/VersionComparatorSpec.scala
+++ core/java/test/scalatest/net/i2p/util/VersionComparatorSpec.scala
@@ -16,6 +16,15 @@ class VersionComparatorSpec extends FunSpec with ShouldMatchers {
         it("should find that 0.1.3 is greater than 0.1.2") {
             vc.compare("0.1.3", "0.1.2") should equal (1)
         }
+        it("should find that 0.1.2.3.4 is greater than 0.1.2") {
+            vc.compare("0.1.2.3.4", "0.1.2") should equal (1)
+        }
+        it("should find that 0.1.2 is less than 0.1.2.3.4") {
+            vc.compare("0.1.2", "0.1.2.3.4") should equal (-1)
+        }
+        it("should find that 0.1.3 is greater than 0.1.2.3.4") {
+            vc.compare("0.1.3", "0.1.2.3.4") should equal (1)
+        }
         it("should find that 0.1.2 is equal to 0-1-2") {
             vc.compare("0.1.2", "0-1-2") should equal (0)
         }
@@ -24,6 +33,15 @@ class VersionComparatorSpec extends FunSpec with ShouldMatchers {
         }
         it("should find that 0.1.2-foo is equal to 0.1.2-bar") {
             vc.compare("0.1.2-foo", "0.1.2-bar") should equal (0)
+        }
+        it("should find that -0.1.2 is less than -0.1.3") {
+            vc.compare("-0.1.2", "-0.1.3") should equal (-1)
+        }
+        it("should find that 0..2 is greater than 0.1.2") {
+            vc.compare("0..2", "0.1.2") should equal (1)
+        }
+        it("should find that 0.1.2 is less than 0..2") {
+            vc.compare("0.1.2", "0..2") should equal (-1)
         }
     }
 }
