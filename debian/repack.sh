8c13d3203667c2b9e55396001ccae4ef8a5ec23c
kytv <kytv@mail.i2p>
2012-12-23 17:20:15 +0000

    debian: add repack script and update watch file to use repack.sh

---
+++ debian/repack.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Strip out binaries from the official upstream I2P source tarballs that are
+# not required to build the Debian packages.
+
+# Script mostly borrowed from Raphael Geissert's dfsg-repack.sh
+
+
+set -e
+
+if [ ! -f "$3" ] && [ ! -f "$1" ]; then
+    echo "ERROR: This script must be run via uscan or by manually specifying the input tarball." >&2
+    exit 1
+fi
+
+tarball=
+
+[ -f "$3" ] && tarball="$3"
+[ -z "$tarball" -a -f "$1" ] && tarball="$1"
+
+fname=$(basename "$tarball" .bz2)
+tarball=$(readlink -f "$tarball")
+
+tdir=$(mktemp -d)
+trap '[ ! -d "$tdir" ] || rm -r "$tdir"' EXIT
+
+cp -f ${tarball} "$tarball.bkp"
+echo "Filtering tarball contents..."
+bzcat "$tarball" | tar --wildcards --delete '*/installer/lib/*' \
+                        --delete '*/Slackware/*' \
+                        --delete '*/debian/*' > "$tdir/${fname}"
+
+echo "Compressing filtered tarball..."
+bzip2 -9 "$tdir/${fname}"
+
+repackedtarball=$(echo $tarball|sed -e 's/i2psource/i2p/' -e 's/\.orig\.tar\.bz2/+repack.orig.tar.bz2/')
+mv "$tdir/${fname}.bz2" "$repackedtarball"
+echo "Repacked tarball saved to $repackedtarball."
