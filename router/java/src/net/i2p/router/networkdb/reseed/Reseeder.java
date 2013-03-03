b2f1e78d625e539a821688adfe7b07f19f2d1b1a
meeh <meeh@mail.i2p>
2013-03-03 17:08:26 +0000

    Temporary removing ju1c3d's reseed server until it's up and working fine on the new system.

--- router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
+++ router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
@@ -69,8 +69,7 @@ public class Reseeder {
               "http://193.150.121.66/netDb/" + "," +
               "http://netdb.i2p2.no/" + "," +
               "http://reseed.info/"  + "," +
-              "http://i2p.feared.eu/" + "," +
-              "http://netdb2.i2p2.no/";
+              "http://i2p.feared.eu/";
 
     /** @since 0.8.2 */
     public static final String DEFAULT_SSL_SEED_URL =
@@ -82,8 +81,7 @@ public class Reseeder {
               "https://193.150.121.66/netDb/" + "," +
               "https://netdb.i2p2.no/" + "," +
               "https://reseed.info/"  + "," +
-              "https://i2p.feared.eu/" + "," +
-              "https://netdb2.i2p2.no/";
+              "https://i2p.feared.eu/";
 
     public static final String PROP_PROXY_HOST = "router.reseedProxyHost";
     public static final String PROP_PROXY_PORT = "router.reseedProxyPort";
