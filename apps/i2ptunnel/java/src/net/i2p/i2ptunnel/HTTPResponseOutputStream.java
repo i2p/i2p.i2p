aa547a161085f35d7c9f2627a648ce705ee24fe3
zzz <zzz@mail.i2p>
2013-04-23 18:23:38 +0000

    * i2ptunnel: Block b32.i2p supercookies

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/HTTPResponseOutputStream.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/HTTPResponseOutputStream.java
@@ -193,6 +193,17 @@ class HTTPResponseOutputStream extends FilterOutputStream {
                                 } else if ("content-type".equals(lcKey)) {
                                     // save for compress decision on server side
                                     _contentType = val;
+                                } else if ("set-cookie".equals(lcKey)) {
+                                    String lcVal = val.toLowerCase(Locale.US);
+                                    if (lcVal.contains("domain=b32.i2p") ||
+                                        lcVal.contains("domain=.b32.i2p")) {
+                                        // Strip privacy-damaging "supercookie" for b32.i2p
+                                        // Let's presume the user agent ignores a cookie for "i2p"
+                                        // See RFC 6265 and http://publicsuffix.org/
+                                        if (_log.shouldLog(Log.INFO))
+                                            _log.info("Stripping \"" + key + ": " + val + "\" from response ");
+                                        break;
+                                    }
                                 }
                                 out.write((key.trim() + ": " + val.trim() + "\r\n").getBytes());
                             }
