3b7daafad7311477ae6f7d09d45566a9597fc720
str4d <str4d@mail.i2p>
2013-06-09 12:21:35 +0000

    Fill in basic datastructure length tests

--- core/java/test/scalatest/net/i2p/data/SessionKeySpec.scala
+++ core/java/test/scalatest/net/i2p/data/SessionKeySpec.scala
@@ -7,7 +7,11 @@ import org.scalatest.matchers.ShouldMatchers
  * @author str4d
  */
 class SessionKeySpec extends FunSpec with ShouldMatchers {
+    val sessionKey = new SessionKey
+
     describe("A SessionKey") {
-        it("should be 32 bytes long") (pending)
+        it("should be 32 bytes long") {
+            sessionKey should have length (32)
+        }
     }
 }
