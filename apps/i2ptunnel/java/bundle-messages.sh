968b9a0304b9b8f274c2c50d631bed8918f3aaec
kytv <kytv@mail.i2p>
2013-09-20 01:38:20 +0000

    fix previous check-in

--- apps/i2ptunnel/java/bundle-messages.sh
+++ apps/i2ptunnel/java/bundle-messages.sh
@@ -14,8 +14,8 @@ CLASS=net.i2p.i2ptunnel.web.messages
 TMPFILE=build/javafiles.txt
 export TZ=UTC
 RC=0
-if ! $(which javac}; then
-    export JAVAC=${JAVA_HOME}/../bin/javac
+if ! $(which javac); then
+    export JAVAC="${JAVA_HOME}/../bin/javac"
 fi
 
 if [ "$1" = "-p" ]
