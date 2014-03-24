a974268e7b08ee43f990471b99bafdd1d4ede458
kytv <kytv@mail.i2p>
2014-03-24 11:57:04 +0000

    postinstall: fix freebsd x86 wrapper path
    Spotted in the FreeBSD Port (http://svnweb.freebsd.org/ports/head/security/i2p/files/patch-installer__resources__postinstall.sh?view=markup)

--- installer/resources/postinstall.sh
+++ installer/resources/postinstall.sh
@@ -72,7 +72,7 @@ case $HOST_OS in
             wrapperpath="./lib/wrapper/freebsd64"
             cp ${wrapperpath}/libwrapper.so ./lib/
             # the 32bit libwrapper.so will be needed if a 32 bit jvm is used
-            cp ./lib/freebsd/libwrapper.so ./lib/libwrapper-freebsd-x86-32.so
+            cp ./lib/wrapper/freebsd/libwrapper.so ./lib/libwrapper-freebsd-x86-32.so
         fi
         ;;
     osx )
