5d5a68cb3e99980bcbb4dedaedd81ea9f5a89cc8
zzz <zzz@mail.i2p>
2014-09-02 14:11:22 +0000

    * CryptoChecker: Log tweaks, handle gij

--- router/java/src/net/i2p/router/tasks/CryptoChecker.java
+++ router/java/src/net/i2p/router/tasks/CryptoChecker.java
@@ -1,7 +1,9 @@
 package net.i2p.router.tasks;
 
+import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 import net.i2p.crypto.SigType;
 import net.i2p.router.RouterContext;
@@ -16,8 +18,9 @@ import net.i2p.util.SystemVersion;
 public class CryptoChecker {
 
     private static String JRE6 = "http://www.oracle.com/technetwork/java/javase/downloads/index.html";
-    private static String JRE7 = "http://www.oracle.com/technetwork/java/javase/documentation/java-se-7-doc-download-435117.html";
-    private static String JRE8 = "http://www.oracle.com/technetwork/java/javase/documentation/jdk8-doc-downloads-2133158.html";
+    // these two are US-only and can change?
+    //private static String JRE7 = "http://www.oracle.com/technetwork/java/javase/documentation/java-se-7-doc-download-435117.html";
+    //private static String JRE8 = "http://www.oracle.com/technetwork/java/javase/documentation/jdk8-doc-downloads-2133158.html";
 
     public static void warnUnavailableCrypto(RouterContext ctx) {
         if (SystemVersion.isAndroid())
@@ -43,11 +46,11 @@ public class CryptoChecker {
             }
             if (!isUnlimited()) {
                 String s = "Please consider installing the Java Cryptography Unlimited Strength Jurisdiction Policy Files from ";
-                if (SystemVersion.isJava8())
-                    s  += JRE8;
-                else if (SystemVersion.isJava7())
-                    s  += JRE7;
-                else
+                //if (SystemVersion.isJava8())
+                //    s  += JRE8;
+                //else if (SystemVersion.isJava7())
+                //    s  += JRE7;
+                //else
                     s  += JRE6;
                 log.logAlways(log.WARN, s);
                 System.out.println(s);
@@ -59,9 +62,7 @@ public class CryptoChecker {
     }
 
     /**
-     *  Following code adapted from Orchid
-     *  According to http://www.subgraph.com/orchid.html ,
-     *  "Orchid is licensed under a three-clause BSD license."
+     *  Copied from CryptixAESEngine
      */
     private static boolean isUnlimited() {
         try {
@@ -70,6 +71,14 @@ public class CryptoChecker {
         } catch (NoSuchAlgorithmException e) {
             return false;
         } catch (NoSuchMethodError e) {
+            // JamVM, gij
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+                SecretKeySpec key = new SecretKeySpec(new byte[32], "AES");
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+            } catch (GeneralSecurityException gse) {
+                return false;
+            }
         }
         return true;
     }
