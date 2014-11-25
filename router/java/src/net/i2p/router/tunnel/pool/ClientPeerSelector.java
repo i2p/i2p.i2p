5383f9f097d221e578d213a098a74eb53b04ea96
zzz <zzz@mail.i2p>
2014-11-25 14:25:42 +0000

    Profiles: Change slice selection argument from an int to an enum for clarity

--- router/java/src/net/i2p/router/tunnel/pool/ClientPeerSelector.java
+++ router/java/src/net/i2p/router/tunnel/pool/ClientPeerSelector.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import net.i2p.data.Hash;
 import net.i2p.router.RouterContext;
 import net.i2p.router.TunnelPoolSettings;
+import static net.i2p.router.peermanager.ProfileOrganizer.Slice.*;
 
 /**
  * Pick peers randomly out of the fast pool, and put them into tunnels
@@ -49,7 +50,7 @@ class ClientPeerSelector extends TunnelPeerSelector {
                 if (!settings.isInbound()) {
                     // exclude existing OBEPs to get some diversity
                 }
-                ctx.profileOrganizer().selectFastPeers(1, exclude, matches, settings.getRandomKey(), length == 2 ? 2 : 4);
+                ctx.profileOrganizer().selectFastPeers(1, exclude, matches, settings.getRandomKey(), length == 2 ? SLICE_0_1 : SLICE_0);
                 matches.remove(ctx.routerHash());
                 exclude.addAll(matches);
                 rv.addAll(matches);
@@ -57,7 +58,7 @@ class ClientPeerSelector extends TunnelPeerSelector {
                 if (length > 2) {
                     // middle hop(s)
                     // group 2 or 3
-                    ctx.profileOrganizer().selectFastPeers(length - 2, exclude, matches, settings.getRandomKey(), 3);
+                    ctx.profileOrganizer().selectFastPeers(length - 2, exclude, matches, settings.getRandomKey(), SLICE_2_3);
                     matches.remove(ctx.routerHash());
                     if (matches.size() > 1) {
                         // order the middle peers for tunnels >= 4 hops
@@ -75,7 +76,7 @@ class ClientPeerSelector extends TunnelPeerSelector {
                 if (settings.isInbound()) {
                     // exclude existing IBGWs to get some diversity
                 }
-                ctx.profileOrganizer().selectFastPeers(1, exclude, matches, settings.getRandomKey(), length == 2 ? 3 : 5);
+                ctx.profileOrganizer().selectFastPeers(1, exclude, matches, settings.getRandomKey(), length == 2 ? SLICE_2_3 : SLICE_1);
                 matches.remove(ctx.routerHash());
                 rv.addAll(matches);
             }
