45bc533e38913d107042b047ce52c85a89cb0aa9
dev <dev@robertfoss.se>
2015-04-04 13:37:18 +0000

    findbugs fix: equals() used to compare array and nonarray

--- core/java/src/net/i2p/util/I2PSSLSocketFactory.java
+++ core/java/src/net/i2p/util/I2PSSLSocketFactory.java
@@ -266,7 +266,7 @@ public class I2PSSLSocketFactory {
         for (int i = 0; i < p.length; i++) {
             // if we left SSLv3 in there, we don't support TLS,
             // so we should't remove the SSL ciphers
-            if (p.equals("SSLv3"))
+            if (p[i].equals("SSLv3"))
                 return;
         }
         socket.setEnabledProtocols(p);
