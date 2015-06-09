e067761947ae22a47280ce81a08abd98698196dc
dev <dev@robertfoss.se>
2015-06-09 03:57:44 +0000

    Added a new flavour of checkAvailable() to UpdateManager interface.

--- core/java/src/net/i2p/update/UpdateManager.java
+++ core/java/src/net/i2p/update/UpdateManager.java
@@ -127,6 +127,16 @@ public interface UpdateManager {
      *  Blocking.
      *  An available update may still have a constraint or lack sources.
      *  @param type the UpdateType of this request
+     *  @return new version or null if nothing newer is available
+     *  @since 0.9.21
+     */
+    public String checkAvailable(UpdateType type);
+    
+    /**
+     *  Is an update available?
+     *  Blocking.
+     *  An available update may still have a constraint or lack sources.
+     *  @param type the UpdateType of this request
      *  @param maxWait max time to block
      *  @return new version or null if nothing newer is available
      *  @since 0.9.21
