d662514f74e7e20a060c58ecaa58c78e8a58724b
str4d <str4d@mail.i2p>
2015-07-25 11:42:53 +0000

    Move streaming demo out of tests

--- apps/ministreaming/java/demo/net/i2p/client/streaming/StreamSinkTestServer.java
+++ apps/ministreaming/java/demo/net/i2p/client/streaming/StreamSinkTestServer.java
@@ -1,14 +1,11 @@
-package net.i2p.client.streaming.impl;
+package net.i2p.client.streaming;
 
-import net.i2p.client.streaming.I2PSocketManagerFactory;
 import net.i2p.client.streaming.StreamSinkServer;
-import net.i2p.client.streaming.impl.I2PSocketManagerFull;
 /**
  *
  */
 public class StreamSinkTestServer {
     public static void main(String args[]) {
-        System.setProperty(I2PSocketManagerFactory.PROP_MANAGER, I2PSocketManagerFull.class.getName());
         //System.setProperty(I2PClient.PROP_TCP_HOST, "dev.i2p.net");
         //System.setProperty(I2PClient.PROP_TCP_PORT, "4101");
         System.setProperty("tunnels.depthInbound", "0");
