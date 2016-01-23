e7081491ca49ec27d41eb252b30306167e396e77
zzz <zzz@mail.i2p>
2016-01-23 17:58:16 +0000

    Fix unit tests I broke

--- router/java/src/net/i2p/router/tunnel/HopProcessor.java
+++ router/java/src/net/i2p/router/tunnel/HopProcessor.java
@@ -30,13 +30,27 @@ class HopProcessor {
      */
     //static final boolean USE_DOUBLE_IV_ENCRYPTION = true;
     static final int IV_LENGTH = 16;
-    
 
+    /**
+     *  @deprecated used only by unit tests
+     */
+    HopProcessor(I2PAppContext ctx, HopConfig config) {
+        this(ctx, config, createValidator());
+    }
+    
     public HopProcessor(I2PAppContext ctx, HopConfig config, IVValidator validator) {
         _context = ctx;
         _log = ctx.logManager().getLog(HopProcessor.class);
         _config = config;
         _validator = validator;
+    }
+
+    /**
+     *  @deprecated used only by unit test constructor
+     */
+    private static IVValidator createValidator() { 
+        // yeah, we'll use an O(1) validator later (e.g. bloom filter)
+        return new HashSetIVValidator();
     }
     
     /**
