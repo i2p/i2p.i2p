26a24a98ed18678bbbe5e0cb1a81427c1466cf23
zzz <zzz@mail.i2p>
2016-11-06 15:31:01 +0000

    add java 9 log warning

--- apps/routerconsole/java/src/net/i2p/router/web/RouterConsoleRunner.java
+++ apps/routerconsole/java/src/net/i2p/router/web/RouterConsoleRunner.java
@@ -306,7 +306,8 @@ public class RouterConsoleRunner implements RouterApp {
         boolean noPack200 = (PluginStarter.pluginsEnabled(_context) || !NewsHelper.isUpdateDisabled(_context)) &&
                             !FileUtil.isPack200Supported();
         boolean openARM = SystemVersion.isARM() && SystemVersion.isOpenJDK();
-        if (noJava7 || noPack200 || openARM) {
+        boolean isJava9 = SystemVersion.isJava9();
+        if (noJava7 || noPack200 || openARM || isJava9) {
             String s = "Java version: " + System.getProperty("java.version") +
                        " OS: " + System.getProperty("os.name") + ' ' +
                        System.getProperty("os.arch") + ' ' +
@@ -326,6 +327,11 @@ public class RouterConsoleRunner implements RouterApp {
             }
             if (openARM) {
                 s = "OpenJDK is not recommended for ARM. Use Oracle Java 8";
+                log.logAlways(net.i2p.util.Log.WARN, s);
+                System.out.println("Warning: " + s);
+            }
+            if (isJava9) {
+                s = "Java 9 support is beta, and not recommended for general use";
                 log.logAlways(net.i2p.util.Log.WARN, s);
                 System.out.println("Warning: " + s);
             }
