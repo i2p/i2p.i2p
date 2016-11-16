68e5fd6d087bdce826f9a09d43e96f258368a404
zzz <zzz@mail.i2p>
2016-11-16 18:05:40 +0000

    Profiles: Pull same-IP detection into a utility class, for use by netdb

---
+++ router/java/src/net/i2p/router/util/MaskedIPSet.java
@@ -0,0 +1,92 @@
+package net.i2p.router.util;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.i2p.data.DataHelper;
+import net.i2p.data.Hash;
+import net.i2p.data.router.RouterAddress;
+import net.i2p.data.router.RouterInfo;
+import net.i2p.router.RouterContext;
+
+/**
+ *  Used for detection of routers with matching IPs or family.
+ *  Moved out of ProfileOrganizer for use in netdb also.
+ *
+ *  @since 0.9.28
+ */
+public class MaskedIPSet extends HashSet<String> {
+
+    public MaskedIPSet() {
+        super();
+    }
+
+    public MaskedIPSet(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    /**
+      * The Set of IPs for this peer, with a given mask.
+      * Includes the comm system's record of the IP, and all netDb addresses.
+      *
+      * As of 0.9.24, returned set will include netdb family as well.
+      *
+      * @param mask is 1-4 (number of bytes to match)
+      * @return an opaque set of masked IPs for this peer
+      */
+    public MaskedIPSet(RouterContext ctx, Hash peer, int mask) {
+        super(4);
+        byte[] commIP = ctx.commSystem().getIP(peer);
+        if (commIP != null)
+            add(maskedIP(commIP, mask));
+        RouterInfo pinfo = ctx.netDb().lookupRouterInfoLocally(peer);
+        if (pinfo == null)
+            return;
+        Collection<RouterAddress> paddr = pinfo.getAddresses();
+        for (RouterAddress pa : paddr) {
+            byte[] pib = pa.getIP();
+            if (pib == null) continue;
+            add(maskedIP(pib, mask));
+        }
+        String family = pinfo.getOption("family");
+        if (family != null) {
+            // TODO should KNDF put a family-verified indicator in the RI,
+            // after checking the sig, or does it matter?
+            // What's the threat here of not avoid ding a router
+            // falsely claiming to be in the family?
+            // Prefix with something so an IP can't be spoofed
+            add('x' + family);
+        }
+    }
+
+    /**
+     * generate an arbitrary unique value for this ip/mask (mask = 1-4)
+     * If IPv6, force mask = 6.
+     * @param mask is 1-4 (number of bytes to match)
+     */
+    private static String maskedIP(byte[] ip, int mask) {
+        final StringBuilder buf = new StringBuilder(1 + (mask*2));
+        final char delim;
+        if (ip.length == 16) {
+            mask = 6;
+            delim = ':';
+        } else {
+            delim = '.';
+        }
+        buf.append(delim);
+        buf.append(Long.toHexString(DataHelper.fromLong(ip, 0, mask)));
+        return buf.toString();
+    }
+
+    /** does this contain any of the elements in b? */
+    public boolean containsAny(Set<String> b) {
+        if (isEmpty() || b.isEmpty())
+            return false;
+        for (String s : b) {
+            if (contains(s))
+                return true;
+        }
+        return false;
+    }
+}
