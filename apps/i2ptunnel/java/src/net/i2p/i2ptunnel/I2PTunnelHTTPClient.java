04321e6af36960723197449c968ed4d814381e6a
zzz <zzz@mail.i2p>
2016-12-01 14:29:02 +0000

    remove jump server

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClient.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClient.java
@@ -1414,7 +1414,7 @@ public class I2PTunnelHTTPClient extends I2PTunnelHTTPClientBase implements Runn
     }
 
     public static final String DEFAULT_JUMP_SERVERS =
-                               "http://i2host.i2p/cgi-bin/i2hostjump?," +
+            //"http://i2host.i2p/cgi-bin/i2hostjump?," +
             "http://stats.i2p/cgi-bin/jump.cgi?a=," +
 	    "http://no.i2p/jump/," +
 	    "http://i2pjump.i2p/jump/";
