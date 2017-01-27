1f228a3f8586ee451c8b97093f6dd4f0e83d268a
zzz <zzz@mail.i2p>
2017-01-27 19:15:32 +0000

    Build: Rename translated man pages to be compatible with dh_installman for debian. Add i2prouter-nowrapper man page. Misc. man page fixups (man man-pages for the standard) No ant targets or deb packaging yet.

--- installer/resources/poupdate-man.sh
+++ installer/resources/poupdate-man.sh
@@ -2,10 +2,10 @@
 #
 # can't get po4a to do this
 #
-for i in eepget i2prouter
+for i in eepget i2prouter i2prouter-nowrapper
 do
 	for j in es zh
 	do
-		po4a-translate -f man -m man/$i.1 -p locale-man/man_$j.po -l man/$j/$i.1 -L UTF-8 -M UTF-8 -k 10 -v -d
+		po4a-translate -f man -m man/$i.1 -p locale-man/man_$j.po -l man/$i.$j.1 -L UTF-8 -M UTF-8 -k 10 -v -d
 	done
 done
