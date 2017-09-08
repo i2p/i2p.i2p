7346ce1c89580fb5d87027352cb4de73ff741b76
str4d <str4d@mail.i2p>
2017-09-08 10:49:05 +0000

    Enable benchmarks.sh to run with everything in one directory

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
 CWD=$(dirname "$0")
-if [ "x$JBIGI" = 'x' ]
-then
-    JBIGI="$CWD/../build/jbigi.jar"
-fi
 if [ "x$JAVA" = 'x' ]
 then
     JAVA=java
 fi
 
-CLASSPATH="$CWD/java/build/benchmarks.jar"
+if [ "x$BENCHMARKS" = 'x' ]
+then
+    BENCHMARKS="$CWD/benchmarks.jar"
+    stat "$BENCHMARKS" >/dev/null 2>&1
+    if [ "x$?" != 'x0' ]
+    then
+        BENCHMARKS="$CWD/java/build/benchmarks.jar"
+    fi
+fi
+
+if [ "x$JBIGI" = 'x' ]
+then
+    JBIGI="$CWD/jbigi.jar"
+    stat "$JBIGI" >/dev/null 2>&1
+    if [ "x$?" != 'x0' ]
+    then
+        JBIGI="$CWD/../build/jbigi.jar"
+    fi
+fi
+
+CLASSPATH="$BENCHMARKS"
 if [ "x${1:-}" = 'x--jbigi' ]
 then
     CLASSPATH="$CLASSPATH:$JBIGI"
     shift
 fi
 
-$JAVA -cp "$CLASSPATH" org.openjdk.jmh.Main "$@"
+$JAVA -cp "$CLASSPATH" "$@"
