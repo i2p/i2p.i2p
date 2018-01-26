99b028a4e78ad767f6865fbf8c4a9227965d6e1a
zzz <zzz@mail.i2p>
2018-01-26 18:39:48 +0000

    bump for review

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -18,7 +18,7 @@ public class RouterVersion {
     /** deprecated */
     public final static String ID = "Monotone";
     public final static String VERSION = CoreVersion.VERSION;
-    public final static long BUILD = 27;
+    public final static long BUILD = 28;
 
     /** for example "-test" */
     public final static String EXTRA = "-rc";
