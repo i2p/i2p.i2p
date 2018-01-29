23aaefa98613ad81f6628c039a02c99f8464b586
zzz <zzz@mail.i2p>
2018-01-29 21:06:02 +0000

    0.9.33

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -18,7 +18,7 @@ public class CoreVersion {
     /** deprecated */
     public final static String ID = "Monotone";
 
-    public final static String VERSION = "0.9.32";
+    public final static String VERSION = "0.9.33";
 
     /**
      *  For Vuze.
