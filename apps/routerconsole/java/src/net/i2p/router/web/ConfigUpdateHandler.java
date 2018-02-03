e01234c6a4d9377337073887a5e9c1d43f044bd2
zzz <zzz@mail.i2p>
2018-02-03 16:41:34 +0000

    Update the update URLs

--- apps/routerconsole/java/src/net/i2p/router/web/ConfigUpdateHandler.java
+++ apps/routerconsole/java/src/net/i2p/router/web/ConfigUpdateHandler.java
@@ -83,21 +83,21 @@ public class ConfigUpdateHandler extends FormHandler {
      */
     private static final String PACK200_URLS =
     "http://echelon.i2p/i2p/i2pupdate.su2\r\n" +
-    "http://inr.i2p/i2p/i2pupdate.su2\r\n" +
+    //"http://inr.i2p/i2p/i2pupdate.su2\r\n" +
     //"http://meeh.i2p/i2pupdate/i2pupdate.su2\r\n" +
-    "http://stats.i2p/i2p/i2pupdate.su2\r\n" +
+    "http://stats.i2p/i2p/i2pupdate.su2";
     // "http://www.i2p2.i2p/_static/i2pupdate.su2\r\n" +
-    "http://update.dg.i2p/files/i2pupdate.su2";
+    //"http://update.dg.i2p/files/i2pupdate.su2";
     //"http://update.killyourtv.i2p/i2pupdate.su2\r\n" ;
     // "http://update.postman.i2p/i2pupdate.su2" ;
 
     private static final String NO_PACK200_URLS =
     "http://echelon.i2p/i2p/i2pupdate.sud\r\n" +
-    "http://inr.i2p/i2p/i2pupdate.sud\r\n" +
+    //"http://inr.i2p/i2p/i2pupdate.sud\r\n" +
     //"http://meeh.i2p/i2pupdate/i2pupdate.sud\r\n" +
-    "http://stats.i2p/i2p/i2pupdate.sud\r\n" +
+    "http://stats.i2p/i2p/i2pupdate.sud";
     // "http://www.i2p2.i2p/_static/i2pupdate.sud\r\n" +
-    "http://update.dg.i2p/files/i2pupdate.sud";
+    //"http://update.dg.i2p/files/i2pupdate.sud";
     //"http://update.killyourtv.i2p/i2pupdate.sud\r\n" ;
     // "http://update.postman.i2p/i2pupdate.sud" ;
 
@@ -127,13 +127,15 @@ public class ConfigUpdateHandler extends FormHandler {
 
     private static final String DEFAULT_SU3_UPDATE_URLS =
     "http://echelon.i2p/i2p/i2pupdate.su3\r\n" +
-    "http://inr.i2p/i2p/i2pupdate.su3\r\n" +
+    //"http://inr.i2p/i2p/i2pupdate.su3\r\n" +
     //"http://meeh.i2p/i2pupdate/i2pupdate.su3\r\n" +
     "http://stats.i2p/i2p/i2pupdate.su3\r\n" +
     // "http://www.i2p2.i2p/_static/i2pupdate.su3\r\n" +
-    "http://update.dg.i2p/files/i2pupdate.su3";
+    //"http://update.dg.i2p/files/i2pupdate.su3";
     //"http://update.killyourtv.i2p/i2pupdate.su3\r\n" ;
     // "http://update.postman.i2p/i2pupdate.su3" ;
+    // project download server
+    "http://whnxvjwjhzsske5yevyokhskllvtisv5ueokw6yvh6t7zqrpra2q.b32.i2p/current/i2pupdate.su3";
 
     /**
      *  Empty string if disabled. Cannot be overridden by config.
