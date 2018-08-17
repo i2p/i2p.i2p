26189704f3cff72d3b62364f290fd608c4b1828a
slumlord <slumlord@mail.i2p>
2018-08-17 08:37:41 +0000

    Re-add file

---
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -0,0 +1,32 @@
+package net.i2p.router;
+/*
+ * free (adj.): unencumbered; not under the control of others
+ * Written by jrandom in 2003 and released into the public domain 
+ * with no warranty of any kind, either expressed or implied.  
+ * It probably won't make your computer catch on fire, or eat 
+ * your children, but it might.  Use at your own risk.
+ *
+ */
+
+import net.i2p.CoreVersion;
+
+/**
+ * Expose a version string
+ *
+ */
+public class RouterVersion {
+    /** deprecated */
+    public final static String ID = "Monotone";
+    public final static String VERSION = CoreVersion.VERSION;
+    public final static long BUILD = 24;
+
+    /** for example "-test" */
+    public final static String EXTRA = "-rc";
+    public final static String FULL_VERSION = VERSION + "-" + BUILD + EXTRA;
+    public static void main(String args[]) {
+        System.out.println("I2P Router version: " + FULL_VERSION);
+        System.out.println("Router ID: " + RouterVersion.ID);
+        System.out.println("I2P Core version: " + CoreVersion.VERSION);
+        System.out.println("Core ID: " + CoreVersion.ID);
+    }
+}
