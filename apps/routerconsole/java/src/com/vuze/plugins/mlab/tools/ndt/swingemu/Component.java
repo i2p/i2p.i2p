71d8f0e4d4f0f52d57ce7fcdca8c585d9ede625a
zzz <zzz@mail.i2p>
2018-11-13 17:20:33 +0000

    Vuze/BiglyBT Swing emulation layer for NDT Applet. This is a simple shim to allow running the NDT Applet code with minimal modifications. From BiglyBT MLab plugin: Github rev e927dca, Aug. 18, 2018. From https://github.com/BiglySoftware/BiglyBT-plugin-mlab This is for Tcpbw100 from NDT 3.6.2b; not sufficient to compile Tcpbw100 from 3.7.0.2, changes to follow. License: GPLv2.

---
+++ apps/routerconsole/java/src/com/vuze/plugins/mlab/tools/ndt/swingemu/Component.java
@@ -0,0 +1,147 @@
+/*
+ * Created on May 20, 2010
+ * Created by Paul Gardner
+ * 
+ * Copyright 2010 Vuze, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+
+package com.vuze.plugins.mlab.tools.ndt.swingemu;
+
+public class 
+Component 
+{
+	public void
+	setTitle(
+		String	s )
+	{
+	}
+	
+	public void
+	add(
+		Component c )
+	{	
+	}
+	
+	public void
+	add( String str, Component c )
+	{	
+	}
+	
+	public void
+	add( int i, Component c )
+	{	
+	}
+	
+	public void
+	setEnabled(
+		boolean	b )
+	{
+	}
+	
+	public void
+	setVisible(
+		boolean b )
+	{
+	}
+	
+	public void
+	setEditable(
+		boolean	b )
+	{	
+	}
+	
+	public void
+	setResizable(
+		boolean	b )
+	{	
+	}
+	
+	public void
+	setSize(
+		int	i, int j )
+	{
+	}
+	
+	public void
+	setPreferredSize(
+		Dimension d )
+	{	
+	}
+	
+	public void
+	setBorder(
+		Component	c )
+	{
+	}
+	
+	public void
+	setLayout(
+		BorderLayout l )
+	{
+	}
+	
+	public void
+	setLayout(
+		BoxLayout l )
+	{
+	}
+	
+	public void
+	setCursor(
+		Cursor c )
+	{	
+	}
+	
+	public void
+	setForeground(
+		Color	c )
+	{
+	}
+	
+	public void
+	pack()
+	{
+		
+	}
+	public void
+	repaint()
+	{
+		
+	}
+	
+	public Toolkit
+	getToolkit()
+	{
+		return( new Toolkit());
+	}
+	
+	public void
+	addMouseListener(
+		MouseAdapter	l )
+	{
+		
+	}
+	
+	public void
+	addWindowListener(
+		WindowAdapter l )
+	{
+		
+	}
+}
