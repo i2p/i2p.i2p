46e31746b464c6b151e941b1787ddd0cd2921aec
zzz <zzz@mail.i2p>
2019-04-25 16:35:19 +0000

    Build: Drop unmaintained sample apparmor script (ticket #2319) We support apparmor for Debian package installs only.

--- installer/resources/postinstall.sh
+++ installer/resources/postinstall.sh
@@ -111,9 +111,6 @@ if [ ! `echo $HOST_OS  |grep osx` ]; then
     rm -f *i2p_service_osx.command
     rm -f net.i2p.router.plist.template
     #rm -f I2P\ Router\ Console.webloc
-else
-    # The example apparmor profile is useless on OSX
-    rm -f ./scripts/home.i2p.i2prouter
 fi
 
 rm -f ./osid
