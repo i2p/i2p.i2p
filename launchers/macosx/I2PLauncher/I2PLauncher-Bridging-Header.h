803447b4c34cfa365edb1d2dd19f20faccca0b5d
meeh <meeh@mail.i2p>
2019-05-02 22:55:04 +0000

    Mac OSX Launcher: Swift - ObjC/C++ bridge header update.

--- launchers/macosx/I2PLauncher/I2PLauncher-Bridging-Header.h
+++ launchers/macosx/I2PLauncher/I2PLauncher-Bridging-Header.h
@@ -1,9 +1,9 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-#import "SBridge.h"
+#import "ObjectiveC/SBridge.h"
 #import "AppleStuffExceptionHandler.h"
-#import "AppDelegate.h"
-#import "RouterTask.h"
-#import "logger_c.h"
+#import "ObjectiveC/AppDelegate.h"
+#import "ObjectiveC/RouterTask.h"
+#import "ObjectiveC/logger_c.h"
 #import <Webkit/Webkit.h>
