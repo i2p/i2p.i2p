11b391bc359731907c58bd7e6ff8dc9e406a2021
zzz <zzz@mail.i2p>
2019-05-18 14:14:08 +0000

    Javadoc fixes from FreeBSD ports https://svnweb.freebsd.org/ports?view=revision&revision=501144

--- apps/i2pcontrol/java/com/thetransactioncompany/jsonrpc2/package-info.java
+++ apps/i2pcontrol/java/com/thetransactioncompany/jsonrpc2/package-info.java
@@ -13,7 +13,7 @@
  * exception of <i>batching / multicall</i>. This feature is deliberately left
  * out as it tends to confuse users (judging by posts in the JSON-RPC forum).
  *
- * <p>See the <a href="http://www.jsonrpc.org/specification"></a>JSON-RPC 2.0
+ * <p>See the <a href="http://www.jsonrpc.org/specification">JSON-RPC 2.0
  * specification</a> for more information or write to the
  * <a href="https://groups.google.com/forum/#!forum/json-rpc">user group</a> if
  * you have questions.
