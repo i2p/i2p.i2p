adba7e4c25f17f2fcac73953eecda8bab4d2a373
zzz <zzz@mail.i2p>
2019-08-19 14:49:57 +0000

    update earliest time

--- core/java/src/net/i2p/time/BuildTime.java
+++ core/java/src/net/i2p/time/BuildTime.java
@@ -34,10 +34,10 @@ public class BuildTime {
     private static final long _latestTime;
     private static final long YEARS_25 = 25L*365*24*60*60*1000;
     /** update this periodically */
-    private static final String EARLIEST = "2019-06-23 12:00:00 UTC";
+    private static final String EARLIEST = "2019-08-17 12:00:00 UTC";
     // fallback if parse fails ticket #1976
     // date -d 201x-xx-xx +%s
-    private static final long EARLIEST_LONG = 1561262400 * 1000L;
+    private static final long EARLIEST_LONG = 1566014400 * 1000L;
 
     static {
         // this is the standard format of build.timestamp as set in the top-level build.xml
