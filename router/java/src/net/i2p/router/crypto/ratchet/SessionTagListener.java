6a47319b6657160c53c2465aaa4fa29be7fcb0b3
zzz <zzz@mail.i2p>
2019-10-24 14:01:53 +0000

    Crypto: SessionTagListener for RatchetTagSet

---
+++ router/java/src/net/i2p/router/crypto/ratchet/SessionTagListener.java
@@ -0,0 +1,23 @@
+package net.i2p.router.crypto.ratchet;
+
+import net.i2p.data.SessionTag;
+
+/**
+ * Something that looks for SessionTags.
+ *
+ * @since 0.9.44
+ */
+interface SessionTagListener {
+
+    /**
+     *  Map the tag to this tagset.
+     *
+     *  @return true if added, false if dup
+     */
+    public boolean addTag(RatchetSessionTag tag, RatchetTagSet ts);
+
+    /**
+     *  Remove the tag associated with this tagset.
+     */
+    public void expireTag(RatchetSessionTag tag, RatchetTagSet ts);
+}
