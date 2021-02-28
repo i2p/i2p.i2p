3c94f529ad34e4bf94c200aeefbbedacfa48858f
idk <hankhill19580@gmail.com>
2021-02-28 10:43:07 -0500

    remove the bashism in travis.sh

--- tests/scripts/travis.sh
+++ tests/scripts/travis.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if [ "$TRAVIS_JDK_VERSION" == "oraclejdk11" ]; then
+if [ "$TRAVIS_JDK_VERSION" = "oraclejdk11" ]; then
   ./gradlew sonarqube codeCoverageReport -Dsonar.verbose=true
 else
   ./gradlew check codeCoverageReport
