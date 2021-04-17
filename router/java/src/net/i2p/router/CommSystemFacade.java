11151464f32cae0edecc350eb86456a0ac81ac5d
zzz <zzz@i2pmail.org>
2021-04-17 14:30:25 -0400

    Router: State transition fixes

--- router/java/src/net/i2p/router/CommSystemFacade.java
+++ router/java/src/net/i2p/router/CommSystemFacade.java
@@ -539,14 +539,13 @@ public abstract class CommSystemFacade implements Service {
 
                         case REJECT_UNSOLICITED:
                         case IPV4_FIREWALLED_IPV6_OK:
-                        case IPV4_DISABLED_IPV6_FIREWALLED:
                             return REJECT_UNSOLICITED;
 
                         case IPV4_DISABLED_IPV6_OK:
                             return IPV4_DISABLED_IPV6_FIREWALLED;
 
                         case DIFFERENT:
-                        case IPV4_SNAT_IPV6_OK:
+                        case IPV4_DISABLED_IPV6_FIREWALLED:
                             return oldStatus;
 
                         // cases where we already knew the IPv6 state only
@@ -565,6 +564,7 @@ public abstract class CommSystemFacade implements Service {
                             return IPV4_DISABLED_IPV6_FIREWALLED;
 
                         case IPV4_SNAT_IPV6_UNKNOWN:
+                        case IPV4_SNAT_IPV6_OK:
                             return DIFFERENT;
 
                         default:
