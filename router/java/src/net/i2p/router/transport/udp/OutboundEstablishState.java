e43810f182258a34ca6d37dd6dac73d559902401
zzz <zzz@i2pmail.org>
2022-04-05 07:49:43 -0400

    SSU2: Don't send our RI after outbound handshake, it's in the handshake already

--- router/java/src/net/i2p/router/transport/udp/OutboundEstablishState.java
+++ router/java/src/net/i2p/router/transport/udp/OutboundEstablishState.java
@@ -257,6 +257,9 @@ class OutboundEstablishState {
             if (m.getType() == DatabaseStoreMessage.MESSAGE_TYPE) {
                DatabaseStoreMessage dsm = (DatabaseStoreMessage) m;
                if (dsm.getKey().equals(_context.routerHash())) {
+                   // version 2 sends our RI in handshake
+                   if (getVersion() > 1)
+                       return;
                    _isFirstMessageOurDSM = true;
                }
            }
@@ -609,8 +612,8 @@ class OutboundEstablishState {
         }
         _confirmedSentCount++;
         _nextSend = _lastSend + delay;
-        if (_log.shouldLog(Log.DEBUG))
-            _log.debug("Send confirm packets, nextSend in " + delay);
+        if (_log.shouldDebug())
+            _log.debug("Send confirm packets, nextSend in " + delay + " on " + this);
         if (_currentState == OutboundState.OB_STATE_UNKNOWN || 
             _currentState == OutboundState.OB_STATE_PENDING_INTRO ||
             _currentState == OutboundState.OB_STATE_INTRODUCED ||
