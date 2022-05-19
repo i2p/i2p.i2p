5f4ad87d109824f318124b4efb0eb5564462ec5b
zzz <zzz@i2pmail.org>
2022-05-19 13:13:16 -0400

    Bump build time

--- core/java/src/net/i2p/time/BuildTime.java
+++ core/java/src/net/i2p/time/BuildTime.java
@@ -34,10 +34,10 @@ public class BuildTime {
     private static final long _latestTime;
     private static final long YEARS_25 = 25L*365*24*60*60*1000;
     /** update this periodically */
-    private static final String EARLIEST = "2022-02-10 12:00:00 UTC";
+    private static final String EARLIEST = "2022-05-19 12:00:00 UTC";
     // fallback if parse fails ticket #1976
     // date -d 202x-xx-xx +%s
-    private static final long EARLIEST_LONG = 1644451200 * 1000L;
+    private static final long EARLIEST_LONG = 1652918400 * 1000L;
 
     static {
         // this is the standard format of build.timestamp as set in the top-level build.xml
