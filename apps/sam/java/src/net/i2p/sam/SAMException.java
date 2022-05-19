ce0c84c5d9e471450b2ec08666318e6ac989cffe
idk <hankhill19580@gmail.com>
2022-05-19 14:37:17 -0400

    make SAMException class public so that it can easily be used by SAM implementers/embedders. Looks like it should have been public, all the constructors are public but the public keyword is missing on the class itself. Make tabs and spacing consistent in that file.

--- apps/sam/java/src/net/i2p/sam/SAMException.java
+++ apps/sam/java/src/net/i2p/sam/SAMException.java
@@ -1,9 +1,9 @@
 package net.i2p.sam;
 /*
  * free (adj.): unencumbered; not under the control of others
- * Written by human in 2004 and released into the public domain 
- * with no warranty of any kind, either expressed or implied.  
- * It probably won't  make your computer catch on fire, or eat 
+ * Written by human in 2004 and released into the public domain
+ * with no warranty of any kind, either expressed or implied.
+ * It probably won't  make your computer catch on fire, or eat
  * your children, but it might.  Use at your own risk.
  *
  */
@@ -13,18 +13,18 @@ package net.i2p.sam;
  *
  * @author human
  */
-class SAMException extends Exception {
+public class SAMException extends Exception {
 
     private static final long serialVersionUID = 1;
 
     public SAMException() {
-    	super();
+        super();
     }
-    
+
     public SAMException(String s) {
-    	super(s);
+        super(s);
     }
-    
+
     /** @since 0.9.14 */
     public SAMException(String s, Throwable cause) {
         super(s, cause);
