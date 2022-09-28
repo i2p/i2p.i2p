0eb96d6ef0261449556ff65f2bb9b018de162c96
zzz <zzz@i2pmail.org>
2022-09-28 09:17:44 -0400

    Installer: Fix error on Windows when username contains a space (gitlab #367)
    in fixperms.bat

--- installer/resources/fixperms2.bat
+++ installer/resources/fixperms2.bat
@@ -15,4 +15,4 @@
 :: Specifying the SID will work on ALL versions of Windows.
 :: List of well-known SIDs at http://support.microsoft.com/kb/243330/en-us
 ::
-echo Y|icacls %1 /grant:r %username%:F *S-1-5-32-545:RX /c /t /q > %1%\fixperms.log
+echo Y|icacls %1 /grant:r "%username%":F *S-1-5-32-545:RX /c /t /q > %1%\fixperms.log
