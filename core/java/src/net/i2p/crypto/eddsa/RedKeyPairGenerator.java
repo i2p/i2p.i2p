2280f3b4cc1f671cf57e535c5b1a0be335a00cf6
James Richardson <j.richardson@psenterprise.com>
2022-11-23 10:50:13 +0000

    Add missing CC0 copyright headers to EdDSA-Java classes

--- core/java/src/net/i2p/crypto/eddsa/RedKeyPairGenerator.java
+++ core/java/src/net/i2p/crypto/eddsa/RedKeyPairGenerator.java
@@ -1,3 +1,14 @@
+/**
+ * EdDSA-Java by str4d
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * EdDSA-Java has waived all copyright and related or neighboring rights
+ * to EdDSA-Java.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ */
 package net.i2p.crypto.eddsa;
 
 import java.security.KeyPair;
