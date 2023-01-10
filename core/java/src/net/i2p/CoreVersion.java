a5d649ba2dd792c3765f745c54103b36cbd5266c
zzz <zzz@i2pmail.org>
2023-01-10 09:32:31 -0500

    2.1.0

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -31,7 +31,7 @@ public class CoreVersion {
      *  Otherwise, the same as PUBLISHED_VERSION.
      *  RouterVersion.FULL_VERSION is suggested for display to the user.
      */
-    public final static String VERSION = "2.0.0";
+    public final static String VERSION = "2.1.0";
 
     /**
      *  The version published in the netdb via StatisticsManager.
@@ -48,7 +48,7 @@ public class CoreVersion {
      *
      *  @since 0.9.46
      */
-    public final static String PUBLISHED_VERSION = "0.9.56";
+    public final static String PUBLISHED_VERSION = "0.9.57";
 
     /**
      *  For Vuze.
