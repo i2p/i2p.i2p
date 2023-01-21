37104e30bfc36518da4b8c79f321891dfa815512
zzz <zzz@i2pmail.org>
2023-01-21 16:38:10 -0500

    i2psnark: More search CSS and js

--- apps/i2psnark/resources/js/search.js
+++ apps/i2psnark/resources/js/search.js
@@ -11,17 +11,30 @@ function initSearch()
 	var sch = document.getElementById("search");
 	if (sch != null) {
 		var box = document.getElementById("searchbox");
+		var cxl = document.getElementById("searchcancel");
 		sch.addEventListener("reset", function(event) {
 			if (box.value !== "") {
 				box.value = "";
 				requestAjax2(-1);
 			}
+			cxl.classList.add("disabled");
 			event.preventDefault();
 		});
 
 		box.addEventListener("input", function(event) {
+			if (box.value !== "") {
+				cxl.classList.remove("disabled");
+			} else {
+				cxl.classList.add("disabled");
+			}
 			requestAjax2(-1);
 		});
+
+		if (box.value !== "") {
+			cxl.classList.remove("disabled");
+		} else {
+			cxl.classList.add("disabled");
+		}
 	}
 }
 
