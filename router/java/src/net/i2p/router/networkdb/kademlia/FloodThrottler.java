cb901393424ddb14654ab4c214d26ad35b9b846c
zzz <zzz@i2pmail.org>
2023-02-11 06:34:04 -0500

    NetDB: Increase flood thottle time

--- router/java/src/net/i2p/router/networkdb/kademlia/FloodThrottler.java
+++ router/java/src/net/i2p/router/networkdb/kademlia/FloodThrottler.java
@@ -14,7 +14,7 @@ import net.i2p.util.SimpleTimer2;
 class FloodThrottler {
     private final ObjectCounter<Hash> counter;
     private static final int MAX_FLOODS = 3;
-    private static final long CLEAN_TIME = 60*1000;
+    private static final long CLEAN_TIME = 90*1000;
 
     FloodThrottler() {
         this.counter = new ObjectCounter<Hash>();
