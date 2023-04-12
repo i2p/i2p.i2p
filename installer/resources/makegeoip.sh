53bae90089affd0b5ef1c806ca5612e69e7cc8ee
idk <hankhill19580@gmail.com>
2023-04-12 21:14:04 +0000

    fix makegeoip script and update geoip data

--- installer/resources/makegeoip.sh
+++ installer/resources/makegeoip.sh
@@ -10,3 +10,9 @@ wget https://download.db-ip.com/free/$DL || exit 1
 mv $FILE ${FILE}.bak
 mv $DL $FILE
 ls -l $FILE
+OLD_HASH=$(sha256sum ./installer/resources/$FILE | cut -f 1 -d ' ')
+NEW_HASH=$(sha256sum ./$FILE | cut -f 1 -d ' ')
+echo $OLD_HASH $NEW_HASH
+if [ $OLD_HASH != $NEW_HASH ]; then
+    cp -v "$FILE" "./installer/resources/$FILE"
+fi
