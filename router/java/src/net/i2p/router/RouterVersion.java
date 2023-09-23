69d5dfc621f82b0f71aeb2a1bf246dafea59dd17
eyedeekay <idk@mulder>
2023-09-22 20:21:51 -0400

    Router: bump build rev

--- router/java/src/net/i2p/router/RouterVersion.java
+++ router/java/src/net/i2p/router/RouterVersion.java
@@ -20,7 +20,7 @@ public class RouterVersion {
     public final static String VERSION = CoreVersion.VERSION;
     /** for example: "beta", "alpha", "rc" */
     public final static String STATUS = "";
-    public final static long BUILD = 1;
+    public final static long BUILD = 2;
     /** for example "-test" */
     public final static String EXTRA = "";
     public final static String FULL_VERSION = VERSION + "-" + STATUS + BUILD + EXTRA;
