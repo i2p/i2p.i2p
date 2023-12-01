26524e3e4bb9d5cba308a65c992a5b4722cf970b
zzz <zzz@i2pmail.org>
2023-12-01 09:14:33 -0500

    Console: Don't lose tabs in log messages

--- apps/routerconsole/java/src/net/i2p/router/web/helpers/LogsHelper.java
+++ apps/routerconsole/java/src/net/i2p/router/web/helpers/LogsHelper.java
@@ -278,6 +278,7 @@ public class LogsHelper extends HelperBase {
                 msg = msg.substring(0, msg.length() - NL.length());
             // replace \n so that exception stack traces will format correctly and will paste nicely into pastebin
             msg = msg.replace("\n", "<br>&nbsp;&nbsp;&nbsp;&nbsp;\n");
+            msg = msg.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
             buf.append("<li>");
             if (colorize) {
                 // TODO this would be a lot easier if LogConsoleBuffer stored LogRecords instead of formatted strings
