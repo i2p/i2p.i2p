78985cd2fdc222786b4d8971547fed745c6a7a9c
eyedeekay <idk@mulder>
2023-12-10 08:45:09 -0500

    Router: Update GeoIP and Build Time

--- core/java/src/net/i2p/time/BuildTime.java
+++ core/java/src/net/i2p/time/BuildTime.java
@@ -34,10 +34,10 @@ public class BuildTime {
     private static final long _latestTime;
     private static final long YEARS_25 = 25L*365*24*60*60*1000;
     /** update this periodically */
-    private static final String EARLIEST = "2023-06-14 12:00:00 UTC";
+    private static final String EARLIEST = "2023-12-10 12:00:00 UTC";
     // fallback if parse fails ticket #1976
     // date -d 202x-xx-xx +%s
-    private static final long EARLIEST_LONG = 1686700800 * 1000L;
+    private static final long EARLIEST_LONG = 1702166400 * 1000L;
 
     static {
         // this is the standard format of build.timestamp as set in the top-level build.xml
