e25ce7fe847b3f17f2979697f9e118209e60adae
zzz <zzz@mail.i2p>
2024-01-10 18:36:11 +0000

    Susimail: Add markdown js for plain text content

---
+++ apps/susimail/src/js/markdown.js
@@ -0,0 +1,20 @@
+/* @license http://www.gnu.org/licenses/gpl-2.0.html GPL-2.0 */
+/* see also licenses/LICENSE-GPLv2.txt */
+
+function initMarkdown() {
+	var mailbodies = document.getElementsByClassName("mailbody");
+	for(index = 0; index < mailbodies.length; index++)
+	{
+		var mailbody = mailbodies[index];
+		if (mailbody.nodeName === "P") {
+			var converter = new Markdown.Converter();
+			mailbody.innerHTML = converter.makeHtml(mailbody.innerHTML);
+		}
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    initMarkdown();
+}, true);
+
+/* @license-end */
