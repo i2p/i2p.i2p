11668696b7b3a78206b5a7eb92c9fe213c8fca98
zzz <zzz@i2pmail.org>
2025-03-07 06:55:15 -0500

    NetDB: Reseed update

--- router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
+++ router/java/src/net/i2p/router/networkdb/reseed/Reseeder.java
@@ -128,8 +128,8 @@ public class Reseeder {
         "https://reseed.i2pgit.org/"          + ',' +   // hankhill19580_at_gmail.com.crt    // CA
         "https://i2p.novg.net/"               + ',' +   // igor_at_novg.net.crt      // CA                         // Java 8+ only
         "https://i2pseed.creativecowpat.net:8443/" + ',' + // creativecowpat_at_mail.i2p.crt // i2pseed.creativecowpat.net.crt // Java 7+
-        "https://reseed.onion.im/"            + ',' +   // lazygravy_at_mail.i2p     // CA                         // Java 8+ only
-        "https://reseed.memcpy.io/";                    // hottuna_at_mail.i2p.crt   // CA                         // SNI required
+        "https://reseed.onion.im/"; //            + ',' +   // lazygravy_at_mail.i2p     // CA                         // Java 8+ only
+        //"https://reseed.memcpy.io/";                    // hottuna_at_mail.i2p.crt   // CA                         // SNI required
 
     private static final String SU3_FILENAME = "i2pseeds.su3";
 
