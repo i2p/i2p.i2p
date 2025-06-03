03b2d0f6fb92c8838fd9184c97c8042dcf3c1e09
zzz <zzz@i2pmail.org>
2025-06-03 10:04:25 -0400

    Prep for custom error pages

--- apps/jetty/java/src/net/i2p/servlet/I2PErrorHandler.java
+++ apps/jetty/java/src/net/i2p/servlet/I2PErrorHandler.java
@@ -1,16 +1,31 @@
 package net.i2p.servlet;
 
-import org.eclipse.jetty.ee8.nested.ErrorHandler;
+import java.io.File;
+
+import org.eclipse.jetty.ee8.servlet.ErrorPageErrorHandler;
+
+import net.i2p.I2PAppContext;
 
 /**
  *  Customize the error page.
  *
  *  @since Jetty 12
  */
-public class I2PErrorHandler extends ErrorHandler
+public class I2PErrorHandler extends ErrorPageErrorHandler
 {
+    private final File _docroot;
+
     public I2PErrorHandler() {
+        this(new File(I2PAppContext.getGlobalContext().getConfigDir(), "eepsite/docroot"));
+    }
+
+    public I2PErrorHandler(String docroot) {
+        this(new File(docroot));
+    }
+
+    public I2PErrorHandler(File docroot) {
         super();
+        _docroot = docroot;
         setShowServlet(false);
         setShowStacks(false);
     }
