b8121336d8020f21a5d54be91aa41b4d2ab9ecc5
zzz <zzz@i2pmail.org>
2025-06-03 10:39:23 -0400

    Custom error pages

--- apps/jetty/java/src/net/i2p/servlet/I2PErrorHandler.java
+++ apps/jetty/java/src/net/i2p/servlet/I2PErrorHandler.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.eclipse.jetty.ee8.servlet.ErrorPageErrorHandler;
 
 import net.i2p.I2PAppContext;
+import net.i2p.util.FileSuffixFilter;
 
 /**
  *  Customize the error page.
@@ -14,6 +15,7 @@ import net.i2p.I2PAppContext;
 public class I2PErrorHandler extends ErrorPageErrorHandler
 {
     private final File _docroot;
+    private static final String RESOURCES = ".resources";
 
     public I2PErrorHandler() {
         this(new File(I2PAppContext.getGlobalContext().getConfigDir(), "eepsite/docroot"));
@@ -28,6 +30,38 @@ public class I2PErrorHandler extends ErrorPageErrorHandler
         _docroot = docroot;
         setShowServlet(false);
         setShowStacks(false);
+        setErrorPages();
+    }
+
+    /*
+     *  Add error pages for any nnn.html files found.
+     *  Also for the special files 000.html (default), 4xx.html, and 5xx.html.
+     */
+    private void setErrorPages() {
+        File dir = new File(_docroot, RESOURCES);
+        if (!dir.isDirectory())
+            return;
+        File[] files = dir.listFiles(new FileSuffixFilter(".html"));
+        if (files == null)
+            return;
+        for (File file : files) {
+            String name = file.getName();
+            if (name.equals("000.html")) {
+                addErrorPage(GLOBAL_ERROR_PAGE, '/' + RESOURCES + "/000.html");
+            } else if (name.equals("4xx.html")) {
+                addErrorPage(400, 499, '/' + RESOURCES + "/4xx.html");
+            } else if (name.equals("5xx.html")) {
+                addErrorPage(500, 599, '/' + RESOURCES + "/5xx.html");
+            } else if (name.length() == 8 && (name.startsWith("4") || name.startsWith("5"))) {
+                int code;
+                try {
+                    code = Integer.parseInt(name.substring(0, 3));
+                } catch (NumberFormatException nfe) {
+                    continue;
+                }
+                addErrorPage(code, '/' + RESOURCES + '/' + name);
+            }
+        }
     }
 
     // TODO Overrides
