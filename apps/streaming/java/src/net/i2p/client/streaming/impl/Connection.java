4e0d67ef237df7e3f49a674e2dbdc76c573c5b59
zzz <zzz@i2pmail.org>
2025-07-01 08:12:41 -0400

    Streaming: Allow configured max window size higher than default
    to allow testing of larger sizes

--- apps/streaming/java/src/net/i2p/client/streaming/impl/Connection.java
+++ apps/streaming/java/src/net/i2p/client/streaming/impl/Connection.java
@@ -110,6 +110,10 @@ class Connection {
     public static final int DEFAULT_CONNECT_TIMEOUT = 60*1000;
     private static final long MAX_CONNECT_TIMEOUT = 2*60*1000;
 
+    /**
+     *  This is the default maximum. See ConnectionOptions.setMaxWindowSize()
+     *  where the configured maximum is enforced.
+     */
     public static final int MAX_WINDOW_SIZE = 128;
     private static final int UNCHOKES_TO_SEND = 8;
 
