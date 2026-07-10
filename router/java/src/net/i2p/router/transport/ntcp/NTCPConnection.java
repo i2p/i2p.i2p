cd4fa7fe69a509577e0399631287013547b08e11
zzz <zzz@i2pmail.org>
2026-07-10 12:40:03 -0400

    Revert: Transport: Release delayed NTCP buffer if connection was closed
    causing "bad size" errors
    reported by: RN

--- router/java/src/net/i2p/router/transport/ntcp/NTCPConnection.java
+++ router/java/src/net/i2p/router/transport/ntcp/NTCPConnection.java
@@ -1015,8 +1015,6 @@ public class NTCPConnection implements Closeable {
             if (!_closed.get()) {
                 _context.statManager().addRateData("ntcp.throttledWriteComplete", (_context.clock().now()-req.getRequestTime()));
                 write(buf);
-            } else {
-                EventPumper.releaseBuf(buf);
             }
         }
     }
