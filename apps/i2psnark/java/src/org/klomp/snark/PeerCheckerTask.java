f3a062ed58a0fb1d515d6efda420d14f1f533988
zzz <zzz@i2pmail.org>
2026-09-15 12:28:37 -0400

    i2psnark: Extend keepalive interval
    from 30 to 60s, only send on inactivity

--- apps/i2psnark/java/src/org/klomp/snark/PeerCheckerTask.java
+++ apps/i2psnark/java/src/org/klomp/snark/PeerCheckerTask.java
@@ -96,7 +96,10 @@ class PeerCheckerTask implements Runnable
                 continue;
               }
 
-            if (peer.getInactiveTime() > peer.getMaxInactiveTime()) {
+            long inactiveTime = peer.getInactiveTime();
+            // note that streaming is configured to disconnect at 4 minutes
+            // so this probably won't happen here.
+            if (inactiveTime > peer.getMaxInactiveTime()) {
                 if (_log.shouldLog(Log.WARN))
                     _log.warn("Disconnecting peer idle " +
                               DataHelper.formatDuration(peer.getInactiveTime()) + ": " + peer);
@@ -231,15 +234,18 @@ class PeerCheckerTask implements Runnable
                   }
               }
             peer.retransmitRequests();
+            int count = _runCount + i;
             // send PEX, about every 12 minutes
-            if (((_runCount + i) % 17) == 0 && !peer.isCompleted())
+            if ((count % 17) == 0 && !peer.isCompleted())
                 coordinator.sendPeers(peer);
             // send Comment Request, about every 30 minutes
-            if (fetchComments && ((_runCount + i) % 47) == 0)
+            if (fetchComments && (count % 47) == 0)
                 coordinator.sendCommentReq(peer);
             // cheap failsafe for seeds connected to seeds, stop pinging and hopefully
             // the inactive checker (above) will eventually disconnect it
-            if (coordinator.getNeededLength() > 0 || !peer.isCompleted())
+            if ((count & 0x01) == 0 &&
+                inactiveTime > 29*1000 &&
+                (coordinator.getNeededLength() > 0 || !peer.isCompleted()))
                 peer.keepAlive();
             // announce them to local tracker (TrackerClient does this too)
             if (dht != null && (_runCount % 5) == 0) {
