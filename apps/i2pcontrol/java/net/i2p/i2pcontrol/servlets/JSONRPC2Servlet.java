ffafa25dea3396455480cc62ee7d19a1f0f56fba
Nicholas Lopez <nico2k4@gmail.com>
2026-09-17 14:18:57 -0700

    Fix: whitespacing issues

--- apps/i2pcontrol/java/net/i2p/i2pcontrol/servlets/JSONRPC2Servlet.java
+++ apps/i2pcontrol/java/net/i2p/i2pcontrol/servlets/JSONRPC2Servlet.java
@@ -24,7 +24,9 @@ import net.i2p.router.RouterContext;
 import net.i2p.util.Log;
 import net.i2p.util.PortMapper;
 import net.i2p.util.RandomSource;
+
 import net.i2p.i2pcontrol.I2PControlVersion;
+import net.i2p.i2pcontrol.security.KeyStoreProvider;
 import net.i2p.i2pcontrol.security.SecurityManager;
 import net.i2p.i2pcontrol.servlets.jsonrpc2handlers.*;
 import net.i2p.i2pcontrol.servlets.configuration.ConfigurationManager;
@@ -44,6 +46,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Base64;
+
 
 /**
  * Provide an JSON-RPC 2.0 API for remote controlling of I2P
@@ -65,7 +68,7 @@ public class JSONRPC2Servlet extends HttpServlet {
     private static final String TOKEN_ATTRIBUTE = "i2pcontrol.token";
 
     /**
-     * Webapp
+     *  Webapp
      */
     public JSONRPC2Servlet() {
         I2PAppContext ctx = I2PAppContext.getGlobalContext();
@@ -75,10 +78,10 @@ public class JSONRPC2Servlet extends HttpServlet {
         File appDir = ctx.getAppDir();
         _conf = new ConfigurationManager(ctx, appDir, false);
         // we don't really need a keystore
-        // File ksDir = new File(ctx.getConfigDir(), "keystore");
-        // ksDir.mkDir();
-        // KeyStoreProvider ksp = new KeyStoreProvider(ksDir.getAbsolutePath());
-        // _secMan = new SecurityManager(ctx, ksp, _conf);
+        //File ksDir = new File(ctx.getConfigDir(), "keystore");
+        //ksDir.mkDir();
+        //KeyStoreProvider ksp = new KeyStoreProvider(ksDir.getAbsolutePath());
+        //_secMan = new SecurityManager(ctx, ksp, _conf);
         _secMan = new SecurityManager(ctx, null, _conf);
         _helper = new JSONRPC2Helper(_secMan);
         _log = ctx.logManager().getLog(JSONRPC2Servlet.class);
@@ -87,7 +90,7 @@ public class JSONRPC2Servlet extends HttpServlet {
     }
 
     /**
-     * Plugin
+     *  Plugin
      */
     public JSONRPC2Servlet(RouterContext ctx, SecurityManager secMan) {
         _context = ctx;
@@ -145,41 +148,33 @@ public class JSONRPC2Servlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         setHeaders(httpServletResponse);
         PrintWriter out = httpServletResponse.getWriter();
-
         out.println("<html><head></head><body>");
         out.println("<p>I2PControl RPC Service version " + I2PControlVersion.VERSION + " : Running");
-        if ("/password".equals(httpServletRequest.getServletPath())) {
+	if ("/password".equals(httpServletRequest.getServletPath())) {
             HttpSession session = httpServletRequest.getSession(true);
-
             out.println("<form method=\"POST\" action=\"password\">");
-
             // generate token
             RandomSource randomSource = _context.random();
             byte[] bytes = new byte[24];
             randomSource.nextBytes(bytes);
-
             String token = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
             session.setAttribute(TOKEN_ATTRIBUTE, token);
             out.println("<input type=\"hidden\" name=\"token\" value=\"" + token + "\" />");
-
             if (_secMan.isDefaultPasswordValid()) {
-                out.println("<p>The current API password is the default, \"" + _secMan.DEFAULT_AUTH_PASSWORD
-                        + "\". You should change it.");
-            } else {
+                out.println("<p>The current API password is the default, \"" + _secMan.DEFAULT_AUTH_PASSWORD + "\". You should change it.");
+            } else {	
                 out.println("<p>Current API password:<input name=\"password\" type=\"password\">");
             }
             out.println("<p>New API password (twice): <input name=\"password2\" type=\"password\"> " +
-                    "<input name=\"password3\" type=\"password\"> " +
-                    "<input name=\"save\" type=\"submit\" value=\"Change API Password\">" +
-                    "<p>If you forget the API password, <a href=\"/configwebapps\">stop jsonrpc</a>, delete the file <tt>"
-                    + _conf.getConfFile() +
-                    "</tt>, and <a href=\"/configwebapps\">restart jsonrpc</a>.");
+                        "<input name=\"password3\" type=\"password\"> " +
+                        "<input name=\"save\" type=\"submit\" value=\"Change API Password\">" +
+                        "<p>If you forget the API password, <a href=\"/configwebapps\">stop jsonrpc</a>, delete the file <tt>" + _conf.getConfFile() +
+                        "</tt>, and <a href=\"/configwebapps\">restart jsonrpc</a>.");
             out.println("</form>");
-        } else {
+        } else {	
             out.println("<p><a href=\"password\">Change API Password</a>");
         }
         out.println("</body></html>");
@@ -187,8 +182,7 @@ public class JSONRPC2Servlet extends HttpServlet {
     }
 
     /** @since 0.12 */
-    private void doPasswordChange(HttpServletRequest req, HttpServletResponse httpServletResponse)
-            throws ServletException, IOException {
+    private void doPasswordChange(HttpServletRequest req, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String token = req.getParameter("token");
         HttpSession session = req.getSession(false);
         Object sessionToken = session != null ? session.getAttribute(TOKEN_ATTRIBUTE) : null;
@@ -203,16 +197,13 @@ public class JSONRPC2Servlet extends HttpServlet {
         setHeaders(httpServletResponse);
         PrintWriter out = httpServletResponse.getWriter();
         out.println("<html><head></head><body>");
-
         String pw = req.getParameter("password");
         if (pw == null)
             pw = ""; // shouldn't do anything if the password is null
         else
             pw = pw.trim();
-
         String pw2 = req.getParameter("password2");
         String pw3 = req.getParameter("password3");
-
         if (pw2 == null || pw3 == null) {
             out.println("<p>Enter new password twice!");
         } else {
@@ -225,7 +216,7 @@ public class JSONRPC2Servlet extends HttpServlet {
             } else if (_secMan.isValid(pw)) {
                 _secMan.setPasswd(pw2);
                 out.println("<p>API Password changed");
-            } else {
+            } else {	
                 out.println("<p>Incorrect old password, not changed");
             }
         }
@@ -235,23 +226,21 @@ public class JSONRPC2Servlet extends HttpServlet {
     }
 
     /**
-     * @since 0.9.48
+     *  @since 0.9.48
      */
     private static void setHeaders(HttpServletResponse resp) {
         resp.setContentType("text/html");
         resp.setHeader("X-Frame-Options", "SAMEORIGIN");
-        resp.setHeader("Content-Security-Policy",
-                "default-src 'self'; style-src 'self'; script-src 'self'; form-action 'self'; frame-ancestors 'self'; object-src 'none'; media-src 'none'");
+        resp.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self'; script-src 'self'; form-action 'self'; frame-ancestors 'self'; object-src 'none'; media-src 'none'");
         resp.setHeader("X-XSS-Protection", "1; mode=block");
         resp.setHeader("X-Content-Type-Options", "nosniff");
         resp.setHeader("Pragma", "no-cache");
-        resp.setHeader("Cache-Control", "no-cache");
+        resp.setHeader("Cache-Control","no-cache");
     }
 
     @Override
-    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
-            throws ServletException, IOException {
-        if ("/password".equals(httpServletRequest.getServletPath())) {
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+	if ("/password".equals(httpServletRequest.getServletPath())) {
             doPasswordChange(httpServletRequest, httpServletResponse);
             return;
         }
@@ -264,14 +253,15 @@ public class JSONRPC2Servlet extends HttpServlet {
             msg = JSONRPC2Message.parse(req);
 
             if (msg instanceof JSONRPC2Request) {
-                jsonResp = disp.process((JSONRPC2Request) msg, null);
+                jsonResp = disp.process((JSONRPC2Request)msg, null);
                 jsonResp.toJSONObject().put("API", I2PControlVersion.API_VERSION);
                 if (_log.shouldDebug()) {
                     _log.debug("Request: " + msg);
                     _log.debug("Response: " + jsonResp);
                 }
-            } else if (msg instanceof JSONRPC2Notification) {
-                disp.process((JSONRPC2Notification) msg, null);
+            }
+            else if (msg instanceof JSONRPC2Notification) {
+                disp.process((JSONRPC2Notification)msg, null);
                 if (_log.shouldDebug())
                     _log.debug("Notification: " + msg);
             }
